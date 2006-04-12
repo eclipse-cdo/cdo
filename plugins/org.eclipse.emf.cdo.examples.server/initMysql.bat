ef571c4d8d8f6f93877d7021e5e435ae11ae382a
Eike Stepper <stepper@esc-net.de>
2006-04-12 21:02:23 +0000

    better sql init scripts.

--- plugins/org.eclipse.emf.cdo.examples.server/initMysql.bat
+++ plugins/org.eclipse.emf.cdo.examples.server/initMysql.bat
@@ -1,3 +1,9 @@
-@echo off
+set ROOTPW=montages
+set USER=test@localhost
+set PASSWORD=test
+set DATABASE=cdoTest
 
-mysql -u test -ptest -e "drop database if exists cdoTest; create database cdoTest" mysql
+mysql -u root -p%ROOTPW% -e "drop database if exists %DATABASE%" mysql
+mysql -u root -p%ROOTPW% -e "create database %DATABASE%" mysql
+mysql -u root -p%ROOTPW% -e "create user %USER% identified by '%PASSWORD%'" mysql
+mysql -u root -p%ROOTPW% -e "grant all privileges on %DATABASE%.* to %USER%" mysql

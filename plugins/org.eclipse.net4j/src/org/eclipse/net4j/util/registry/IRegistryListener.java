74807bd2c88bbc9b7f99272f9a6faff39b5b1fd0
Eike Stepper <stepper@esc-net.de>
2007-03-11 08:55:57 +0000

    *** empty log message ***

--- plugins/org.eclipse.net4j/src/org/eclipse/net4j/util/registry/IRegistryListener.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.net4j.util.registry;
-
-/**
- * @author Eike Stepper
- */
-public interface IRegistryListener<ID, E>
-{
-  public void notifyRegistryEvent(IRegistryEvent<ID, E> event);
-}

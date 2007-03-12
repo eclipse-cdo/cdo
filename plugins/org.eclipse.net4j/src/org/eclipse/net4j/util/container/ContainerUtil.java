4b355ffc2ef80bfb9bcc9859d94c2ef504c28365
Eike Stepper <stepper@esc-net.de>
2007-03-12 17:36:55 +0000

    *** empty log message ***

---
+++ plugins/org.eclipse.net4j/src/org/eclipse/net4j/util/container/ContainerUtil.java
@@ -0,0 +1,33 @@
+/***************************************************************************
+ * Copyright (c) 2004-2007 Eike Stepper, Germany.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *    Eike Stepper - initial API and implementation
+ **************************************************************************/
+package org.eclipse.net4j.util.container;
+
+/**
+ * @author Eike Stepper
+ */
+public final class ContainerUtil
+{
+  private static final Object[] NO_ELEMENTS = new Object[0];
+
+  private ContainerUtil()
+  {
+  }
+
+  public Object[] getElements(Object container)
+  {
+    if (container instanceof IContainer)
+    {
+      return ((IContainer)container).getElements();
+    }
+
+    return NO_ELEMENTS;
+  }
+}
---
+++ plugins/org.eclipse.net4j/src/org/eclipse/net4j/util/registry/RegistryUtil.java
@@ -0,0 +1,33 @@
+/***************************************************************************
+ * Copyright (c) 2004-2007 Eike Stepper, Germany.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *    Eike Stepper - initial API and implementation
+ **************************************************************************/
+package org.eclipse.net4j.util.registry;
+
+import org.eclipse.internal.net4j.util.registry.UnmodifiableRegistry;
+
+/**
+ * @author Eike Stepper
+ */
+public final class RegistryUtil
+{
+  private RegistryUtil()
+  {
+  }
+
+  public <K, V> IRegistry<K, V> unmodifiableRegistry(IRegistry<K, V> registry)
+  {
+    if (registry instanceof UnmodifiableRegistry)
+    {
+      return registry;
+    }
+
+    return new UnmodifiableRegistry(registry);
+  }
+}

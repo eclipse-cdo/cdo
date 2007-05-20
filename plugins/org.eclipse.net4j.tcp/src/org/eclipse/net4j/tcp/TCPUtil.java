35f3266e51977a97666042b22f1a2399f36d166e
Eike Stepper <stepper@esc-net.de>
2007-05-20 05:24:27 +0000

    *** empty log message ***

---
+++ plugins/org.eclipse.net4j.tcp/src/org/eclipse/net4j/tcp/TCPUtil.java
@@ -0,0 +1,36 @@
+/***************************************************************************
+ * Copyright (c) 2004 - 2007 Eike Stepper, Germany.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *    Eike Stepper - initial API and implementation
+ **************************************************************************/
+package org.eclipse.net4j.tcp;
+
+import org.eclipse.net4j.internal.tcp.TCPAcceptorFactory;
+import org.eclipse.net4j.internal.tcp.TCPConnectorFactory;
+import org.eclipse.net4j.internal.tcp.TCPSelectorFactory;
+import org.eclipse.net4j.internal.tcp.TCPSelectorInjector;
+import org.eclipse.net4j.util.container.IManagedContainer;
+
+/**
+ * @author Eike Stepper
+ * @since 0.8.0
+ */
+public final class TCPUtil
+{
+  private TCPUtil()
+  {
+  }
+
+  public static void prepareContainer(IManagedContainer container)
+  {
+    container.registerFactory(new TCPSelectorFactory());
+    container.registerFactory(new TCPAcceptorFactory());
+    container.registerFactory(new TCPConnectorFactory());
+    container.addPostProcessor(new TCPSelectorInjector());
+  }
+}

1fb2e7be9e90c69fdcbf67d2af2e1bf8785232ec
Eike Stepper <stepper@esc-net.de>
2007-07-27 10:57:52 +0000

    *** empty log message ***

---
+++ plugins/org.eclipse.emf.cdo/src/org/eclipse/emf/cdo/weaver/IPersistenceCallback.java
@@ -0,0 +1,23 @@
+/***************************************************************************
+ * Copyright (c) 2004 - 2007 Eike Stepper, Germany.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *    Eike Stepper - initial API and implementation
+ **************************************************************************/
+package org.eclipse.emf.cdo.weaver;
+
+import org.eclipse.emf.ecore.impl.EObjectImpl;
+
+/**
+ * @author Eike Stepper
+ */
+public interface IPersistenceCallback
+{
+  public void read(EObjectImpl eObject);
+
+  public void write(EObjectImpl eObject);
+}

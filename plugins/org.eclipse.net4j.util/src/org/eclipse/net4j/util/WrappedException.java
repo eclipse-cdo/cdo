f8a0974b9e4cedd1a62c99cafdd5e7a460105eac
Eike Stepper <stepper@esc-net.de>
2007-08-01 14:08:10 +0000

    *** empty log message ***

--- plugins/org.eclipse.net4j.util/src/org/eclipse/net4j/util/WrappedException.java
+++ plugins/org.eclipse.net4j.util/src/org/eclipse/net4j/util/WrappedException.java
@@ -1,5 +1,20 @@
+/***************************************************************************
+ * Copyright (c) 2004 - 2007 Eike Stepper, Germany.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *    Eike Stepper - initial API and implementation
+ **************************************************************************/
 package org.eclipse.net4j.util;
 
+/**
+ * TODO Handle IORuntimeException
+ * 
+ * @author Eike Stepper
+ */
 public class WrappedException extends RuntimeException
 {
   private static final long serialVersionUID = 1L;

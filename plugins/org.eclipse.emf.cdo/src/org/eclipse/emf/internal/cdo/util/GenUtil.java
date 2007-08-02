409ea547b642a1748d6c7861133c14729a1860c8
Eike Stepper <stepper@esc-net.de>
2007-08-02 06:55:17 +0000

    *** empty log message ***

--- plugins/org.eclipse.emf.cdo/src/org/eclipse/emf/internal/cdo/util/GenUtil.java
+++ plugins/org.eclipse.emf.cdo/src/org/eclipse/emf/internal/cdo/util/GenUtil.java
@@ -100,8 +100,7 @@ public final class GenUtil
       }
     }
 
-    if (name.length() != 0)
-      parsedName.addAll(parseName(name, '_'));
+    if (name.length() != 0) parsedName.addAll(parseName(name, '_'));
 
     StringBuilder result = new StringBuilder();
 

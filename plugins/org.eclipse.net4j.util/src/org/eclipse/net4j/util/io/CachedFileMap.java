283c4e2688d255b4be62168e785555fab71c022f
Eike Stepper <stepper@esc-net.de>
2007-08-25 16:15:09 +0000

    *** empty log message ***

---
+++ plugins/org.eclipse.net4j.util/src/org/eclipse/net4j/util/io/CachedFileMap.java
@@ -0,0 +1,38 @@
+package org.eclipse.net4j.util.io;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Eike Stepper
+ */
+public abstract class CachedFileMap<K extends Comparable, V> extends SortedFileMap<K, V>
+{
+  private Map<K, V> cache = new HashMap();
+
+  public CachedFileMap(File file, String mode)
+  {
+    super(file, mode);
+  }
+
+  @Override
+  public V get(K key)
+  {
+    V value = cache.get(key);
+    if (value == null)
+    {
+      value = super.get(key);
+      cache.put(key, value);
+    }
+
+    return value;
+  }
+
+  @Override
+  public V put(K key, V value)
+  {
+    cache.put(key, value);
+    return super.put(key, value);
+  }
+}

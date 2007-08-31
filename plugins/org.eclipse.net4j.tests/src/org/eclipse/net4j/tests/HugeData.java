7d1ec9d38e8a20dcdf531f8905966feff04fa1bb
Eike Stepper <stepper@esc-net.de>
2007-08-31 15:26:17 +0000

    [201839] Net4j/CDO Components must comform to incubation rules https://bugs.eclipse.org/bugs/show_bug.cgi?id=201839

--- plugins/org.eclipse.net4j.tests/src/org/eclipse/net4j/tests/HugeData.java
+++ plugins/org.eclipse.net4j.tests/src/org/eclipse/net4j/tests/HugeData.java
@@ -1,5 +1,5 @@
 /***************************************************************************
- * Copyright (c) 2004-2007 Eike Stepper, Germany.
+ * Copyright (c) 2004 - 2007 Eike Stepper, Germany.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -37,7 +37,7 @@ public final class HugeData
   public static String getText()
   {
     return "/***************************************************************************" + NL
-        + " * Copyright (c) 2004-2007 Eike Stepper, Germany." + NL
+        + " * Copyright (c) 2004 - 2007 Eike Stepper, Germany." + NL
         + " * All rights reserved. This program and the accompanying materials" + NL
         + " * are made available under the terms of the Eclipse Public License v1.0" + NL
         + " * which accompanies this distribution, and is available at" + NL

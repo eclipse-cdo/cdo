410cfefd09aadfe7b9763ac739af09eaa37c27d8
Eike Stepper <stepper@esc-net.de>
2007-09-17 20:30:53 +0000

    [203538] Limit memory consumption in CDORevisionResolverImpl https://bugs.eclipse.org/bugs/show_bug.cgi?id=203538

--- plugins/org.eclipse.net4j.util/src/org/eclipse/net4j/util/cache/ICache.java
+++ plugins/org.eclipse.net4j.util/src/org/eclipse/net4j/util/cache/ICache.java
@@ -15,29 +15,10 @@ package org.eclipse.net4j.util.cache;
  */
 public interface ICache
 {
-
   public ICacheMonitor getCacheMonitor();
 
   /**
-   * Returns the number of elements in the cache that would currently be subject to eviction.
-   */
-  public int getEvictableElementCount();
-
-  /**
-   * Returns the average size of an element currently in the cache or <code>UNKNOWN_ELEMENT_SIZE</code> if the
-   * implementor of this method is not able or willing to calculate the average element size.
-   */
-  public int getAverageElementSize();
-
-  /**
-   * Returns the average time in milliseconds needed to reconstruct an element during a cache miss or
-   * <code>UNKNOWN_RECONSTRUCTION_TIME</code> if the implementor of this method is not able or willing to calculate
-   * the average reconstruction time.
-   */
-  public long getAverageReconstructionTime();
-
-  /**
-   * Instructs this cache to evict <b>elementCount</b> elements and return the number of actually evicted elements.
+   * Instructs this cache to evict <b>elementCount</b> elements.
    */
   public void evictElements(int elementCount);
 }

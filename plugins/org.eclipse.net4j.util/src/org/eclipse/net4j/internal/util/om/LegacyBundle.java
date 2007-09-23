2565f19ed42bee0a6b94bce2b31234a6a688479d
Eike Stepper <stepper@esc-net.de>
2007-09-23 08:30:45 +0000

    [204363] MalformedURLException in LegacyBundle.trimSegments() https://bugs.eclipse.org/bugs/show_bug.cgi?id=204363

--- plugins/org.eclipse.net4j.util/src/org/eclipse/net4j/internal/util/om/LegacyBundle.java
+++ plugins/org.eclipse.net4j.util/src/org/eclipse/net4j/internal/util/om/LegacyBundle.java
@@ -191,7 +191,7 @@ public class LegacyBundle extends AbstractBundle
 
   private static URL trimSegments(URL url, int count) throws MalformedURLException
   {
-    System.out.println("trimSegments: " + url);
+    System.out.println("trimSegments: " + url); // XXX
     String path = url.getPath();
     if (path.endsWith("/")) //$NON-NLS-1$
     {

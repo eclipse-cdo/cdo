081a7fd16ce0b9ecaa2d96c0c7fce28c1f52aa93
Eike Stepper <stepper@esc-net.de>
2007-10-07 09:22:08 +0000

    [205651] Implement transaction rollback https://bugs.eclipse.org/bugs/show_bug.cgi?id=205651

--- plugins/org.eclipse.emf.cdo.ui/src/org/eclipse/emf/cdo/internal/ui/actions/RollbackTransactionAction.java
+++ plugins/org.eclipse.emf.cdo.ui/src/org/eclipse/emf/cdo/internal/ui/actions/RollbackTransactionAction.java
@@ -1,8 +1,12 @@
 package org.eclipse.emf.cdo.internal.ui.actions;
 
+import org.eclipse.emf.cdo.CDOTransaction;
 import org.eclipse.emf.cdo.CDOView;
 
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.ui.IWorkbenchPage;
+
+import java.text.MessageFormat;
 
 /**
  * @author Eike Stepper
@@ -17,6 +21,19 @@ public final class RollbackTransactionAction extends ViewAction
   {
     super(page, TITLE + INTERACTIVE, TOOL_TIP, null, view);
     setEnabled(getTransaction().isDirty());
+  }
+
+  @Override
+  protected void preRun() throws Exception
+  {
+    CDOTransaction transaction = (CDOTransaction)getView();
+    String msg = MessageFormat.format("This transaction contains:\n" + "{0} new resources\n" + "{1} new objects\n"
+        + "{2} dirty objects\n\n" + "Are you sure to rollback this transaction", transaction.getNewResources().size(),
+        transaction.getNewObjects().size(), transaction.getDirtyObjects().size());
+    if (!MessageDialog.openQuestion(getShell(), TITLE, msg))
+    {
+      cancel();
+    }
   }
 
   @Override

8f79d8de000feb0d936e5196b38a90c7e7a95f70
Eike Stepper <stepper@esc-net.de>
2008-07-02 18:50:39 +0000

    [238963]  Resource is empty after closing and restarting eclipse https://bugs.eclipse.org/bugs/show_bug.cgi?id=238963

--- plugins/org.eclipse.emf.cdo.tests.hibernate/src/org/eclipse/emf/cdo/tests/hibernate/AllTests.java
+++ plugins/org.eclipse.emf.cdo.tests.hibernate/src/org/eclipse/emf/cdo/tests/hibernate/AllTests.java
@@ -19,6 +19,7 @@ import org.eclipse.emf.cdo.tests.InvalidationTest;
 import org.eclipse.emf.cdo.tests.MangoTest;
 import org.eclipse.emf.cdo.tests.NoLegacyTest;
 import org.eclipse.emf.cdo.tests.NonCDOResourceTest;
+import org.eclipse.emf.cdo.tests.ResourceTest;
 import org.eclipse.emf.cdo.tests.RevisionDeltaTest;
 import org.eclipse.emf.cdo.tests.RevisionHolderTest;
 import org.eclipse.emf.cdo.tests.RollbackTest;
@@ -50,6 +51,7 @@ public class AllTests
     suite.addTestSuite(MangoTest.class);
     suite.addTestSuite(EnumTest.class);
     suite.addTestSuite(NoLegacyTest.class);
+    suite.addTestSuite(ResourceTest.class);
     suite.addTestSuite(NonCDOResourceTest.class);
     suite.addTestSuite(InvalidationTest.class);
     suite.addTestSuite(InitialTest.class);
---
+++ plugins/org.eclipse.emf.cdo.tests/src/org/eclipse/emf/cdo/tests/ResourceTest.java
@@ -0,0 +1,44 @@
+package org.eclipse.emf.cdo.tests;
+
+import org.eclipse.emf.cdo.CDOSession;
+import org.eclipse.emf.cdo.CDOTransaction;
+import org.eclipse.emf.cdo.eresource.CDOResource;
+import org.eclipse.emf.cdo.tests.model1.Model1Factory;
+import org.eclipse.emf.cdo.tests.model1.Product;
+import org.eclipse.emf.cdo.tests.model1.VAT;
+
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EObject;
+
+public class ResourceTest extends AbstractCDOTest
+{
+  /**
+   * https://bugs.eclipse.org/238963
+   */
+  public void testEmptyContents()
+  {
+    {
+      CDOSession session = openModel1Session();
+      CDOTransaction transaction = session.openTransaction();
+      CDOResource resource = transaction.createResource("/my/resource");
+
+      Product p = Model1Factory.eINSTANCE.createProduct();
+      p.setName("test");
+      p.setVat(VAT.VAT0);
+
+      resource.getContents().add(p);
+      transaction.commit();
+
+      assertEquals(1, resource.getContents().size());
+      session.close();
+    }
+
+    CDOSession session = openModel1Session();
+    CDOTransaction transaction = session.openTransaction();
+    CDOResource resource = transaction.getResource("/my/resource");
+    EList<EObject> contents = resource.getContents();
+    int size = contents.size();
+    assertEquals(1, size);
+    session.close();
+  }
+}

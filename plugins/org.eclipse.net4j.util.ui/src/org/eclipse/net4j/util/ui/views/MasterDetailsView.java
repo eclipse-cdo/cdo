cad2db9597192cb4ee1210bc3f03295b64dfa9e9
Eike Stepper <stepper@esc-net.de>
2008-08-03 16:44:06 +0000

    [241121]  Develop POP Process Tooling https://bugs.eclipse.org/bugs/show_bug.cgi?id=241121

---
+++ plugins/org.eclipse.net4j.util.ui/src/org/eclipse/net4j/util/ui/views/MasterDetailsView.java
@@ -0,0 +1,216 @@
+/***************************************************************************
+ * Copyright (c) 2004 - 2008 Eike Stepper, Germany.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *    Eike Stepper - initial API and implementation
+ **************************************************************************/
+package org.eclipse.net4j.util.ui.views;
+
+import org.eclipse.net4j.util.ObjectUtil;
+import org.eclipse.net4j.util.internal.ui.bundle.OM;
+import org.eclipse.net4j.util.ui.widgets.SashComposite;
+
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jface.viewers.StructuredViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CTabFolder;
+import org.eclipse.swt.custom.CTabItem;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+
+/**
+ * @author Eike Stepper
+ * @since 2.0
+ */
+public abstract class MasterDetailsView extends MultiViewersView
+{
+  private StructuredViewer master;
+
+  private CTabFolder detailsFolder;
+
+  private Object currentMasterElement = new Object();
+
+  private String[] detailTitles;
+
+  private CTabItem[] detailItems;
+
+  private StructuredViewer[] details;
+
+  private int currentIndex;
+
+  public MasterDetailsView()
+  {
+  }
+
+  public StructuredViewer getMaster()
+  {
+    return master;
+  }
+
+  @Override
+  protected Control createUI(Composite parent)
+  {
+    SashComposite sash = new SashComposite(parent, SWT.NONE, 10, 50, false)
+    {
+
+      @Override
+      protected Control createControl1(Composite parent)
+      {
+        master = createMaster(parent);
+        return master.getControl();
+      }
+
+      @Override
+      protected Control createControl2(Composite parent)
+      {
+        // Styles: CLOSE, TOP, BOTTOM, FLAT, BORDER, SINGLE, MULTI
+        detailsFolder = new CTabFolder(parent, SWT.BOTTOM | SWT.FLAT);
+        adjustDetails(null);
+        detailsFolder.addSelectionListener(new SelectionAdapter()
+        {
+          @Override
+          public void widgetSelected(SelectionEvent e)
+          {
+            String title = detailsFolder.getSelection().getText();
+            currentIndex = indexOf(detailItems, title);
+          }
+        });
+
+        return detailsFolder;
+      }
+    };
+
+    setCurrentViewer(master);
+    master.addSelectionChangedListener(new ISelectionChangedListener()
+    {
+      public void selectionChanged(SelectionChangedEvent event)
+      {
+        try
+        {
+          masterSelectionChanged(event);
+        }
+        catch (Error ex)
+        {
+          OM.LOG.error(ex);
+          throw ex;
+        }
+        catch (RuntimeException ex)
+        {
+          OM.LOG.error(ex);
+          throw ex;
+        }
+      }
+    });
+
+    return sash;
+  }
+
+  protected void masterSelectionChanged(SelectionChangedEvent event)
+  {
+    IStructuredSelection selection = (IStructuredSelection)master.getSelection();
+    Object masterElement = selection.getFirstElement();
+    adjustDetails(masterElement);
+  }
+
+  protected abstract StructuredViewer createMaster(Composite parent);
+
+  protected abstract String[] getDetailTitles(Object masterElement);
+
+  protected abstract StructuredViewer createDetail(Composite parent, String title);
+
+  private void adjustDetails(Object masterElement)
+  {
+    if (ObjectUtil.equals(masterElement, currentMasterElement))
+    {
+      return;
+    }
+
+    // Temporarily remember old values
+    String oldDetailTitle = detailItems == null || currentIndex < 0 ? null : detailItems[currentIndex].getText();
+    StructuredViewer[] oldDetails = details;
+    CTabItem[] oldDetailItems = detailItems;
+
+    // Initialize new values
+    detailTitles = getDetailTitles(masterElement);
+    details = new StructuredViewer[detailTitles.length];
+    detailItems = new CTabItem[detailTitles.length];
+
+    for (int i = 0; i < detailTitles.length; i++)
+    {
+      String title = detailTitles[i];
+      int oldIndex = indexOf(oldDetailItems, title);
+      if (oldIndex != -1)
+      {
+        details[i] = oldDetails[oldIndex];
+        oldDetailItems[oldIndex].setControl(null);
+        oldDetailItems[oldIndex].dispose();
+        oldDetailItems[oldIndex] = null;
+      }
+      else
+      {
+        details[i] = createDetail(detailsFolder, title);
+      }
+
+      detailItems[i] = new CTabItem(detailsFolder, SWT.NONE, i);
+      detailItems[i].setText(title);
+      detailItems[i].setControl(details[i].getControl());
+    }
+
+    // Cleanup
+    if (oldDetailItems != null)
+    {
+      for (CTabItem item : oldDetailItems)
+      {
+        if (item != null)
+        {
+          item.dispose();
+        }
+      }
+    }
+
+    if (currentMasterElement != masterElement)
+    {
+      currentMasterElement = masterElement;
+      for (StructuredViewer viewer : details)
+      {
+        viewer.setInput(masterElement);
+      }
+    }
+
+    currentIndex = indexOf(detailItems, oldDetailTitle);
+    if (currentIndex == -1 && details.length > 0)
+    {
+      currentIndex = 0;
+    }
+
+    if (currentIndex != -1)
+    {
+      detailsFolder.setSelection(currentIndex);
+    }
+  }
+
+  public static int indexOf(CTabItem[] items, String title)
+  {
+    if (items != null)
+    {
+      for (int i = 0; i < items.length; i++)
+      {
+        CTabItem item = items[i];
+        if (item != null && ObjectUtil.equals(item.getText(), title))
+        {
+          return i;
+        }
+      }
+    }
+
+    return -1;
+  }
+}
---
+++ plugins/org.eclipse.net4j.util.ui/src/org/eclipse/net4j/util/ui/views/MultiViewersView.java
@@ -0,0 +1,316 @@
+/***************************************************************************
+ * Copyright (c) 2004 - 2008 Eike Stepper, Germany.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *    Eike Stepper - initial API and implementation
+ **************************************************************************/
+package org.eclipse.net4j.util.ui.views;
+
+import org.eclipse.net4j.util.internal.ui.bundle.OM;
+import org.eclipse.net4j.util.ui.UIUtil;
+
+import org.eclipse.jface.action.IMenuListener;
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.action.Separator;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.DoubleClickEvent;
+import org.eclipse.jface.viewers.IDoubleClickListener;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.StructuredViewer;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IActionBars;
+import org.eclipse.ui.IWorkbenchActionConstants;
+import org.eclipse.ui.part.ISetSelectionTarget;
+import org.eclipse.ui.part.ViewPart;
+
+/**
+ * @author Eike Stepper
+ * @since 2.0
+ */
+public abstract class MultiViewersView extends ViewPart implements ISetSelectionTarget
+{
+  private Shell shell;
+
+  private StructuredViewer currentViewer;
+
+  public MultiViewersView()
+  {
+  }
+
+  public Shell getShell()
+  {
+    return shell;
+  }
+
+  public StructuredViewer getCurrentViewer()
+  {
+    return currentViewer;
+  }
+
+  public void setCurrentViewer(StructuredViewer viewer)
+  {
+    currentViewer = viewer;
+    getSite().setSelectionProvider(currentViewer);
+    hookContextMenu(currentViewer);
+  }
+
+  @Override
+  public void setFocus()
+  {
+    getCurrentViewer().getControl().setFocus();
+  }
+
+  public void refreshViewer(boolean updateLabels)
+  {
+    refreshElement(null, updateLabels);
+  }
+
+  public void refreshElement(final Object element, final boolean updateLabels)
+  {
+    try
+    {
+      getDisplay().asyncExec(new Runnable()
+      {
+        public void run()
+        {
+          try
+          {
+            if (element != null)
+            {
+              getCurrentViewer().refresh(element, updateLabels);
+            }
+            else
+            {
+              getCurrentViewer().refresh(updateLabels);
+            }
+          }
+          catch (RuntimeException ignore)
+          {
+          }
+        }
+      });
+    }
+    catch (RuntimeException ignore)
+    {
+    }
+  }
+
+  public void updateLabels(final Object element)
+  {
+    try
+    {
+      getDisplay().asyncExec(new Runnable()
+      {
+        public void run()
+        {
+          try
+          {
+            getCurrentViewer().update(element, null);
+          }
+          catch (RuntimeException ignore)
+          {
+          }
+        }
+      });
+    }
+    catch (RuntimeException ignore)
+    {
+    }
+  }
+
+  public void revealElement(final Object element)
+  {
+    try
+    {
+      getDisplay().asyncExec(new Runnable()
+      {
+        public void run()
+        {
+          try
+          {
+            getCurrentViewer().reveal(element);
+          }
+          catch (RuntimeException ignore)
+          {
+          }
+        }
+      });
+    }
+    catch (RuntimeException ignore)
+    {
+    }
+  }
+
+  public void selectReveal(ISelection selection)
+  {
+    getCurrentViewer().setSelection(selection, true);
+  }
+
+  public void closeView()
+  {
+    try
+    {
+      getSite().getShell().getDisplay().syncExec(new Runnable()
+      {
+        public void run()
+        {
+          try
+          {
+            getSite().getPage().hideView(MultiViewersView.this);
+            MultiViewersView.this.dispose();
+          }
+          catch (Exception ignore)
+          {
+          }
+        }
+      });
+    }
+    catch (Exception ignore)
+    {
+    }
+  }
+
+  @Override
+  public final void createPartControl(Composite parent)
+  {
+    try
+    {
+      shell = parent.getShell();
+      Composite composite = UIUtil.createGridComposite(parent, 1);
+
+      Control control = createUI(composite);
+      control.setLayoutData(UIUtil.createGridData());
+
+      hookDoubleClick();
+      contributeToActionBars();
+    }
+    catch (Error ex)
+    {
+      OM.LOG.error(ex);
+      throw ex;
+    }
+    catch (RuntimeException ex)
+    {
+      OM.LOG.error(ex);
+      throw ex;
+    }
+  }
+
+  protected abstract Control createUI(Composite parent);
+
+  protected void doubleClicked(Object object)
+  {
+  }
+
+  protected void fillContextMenu(IMenuManager manager, StructuredViewer viewer, IStructuredSelection selection)
+  {
+  }
+
+  protected void fillLocalPullDown(IMenuManager manager)
+  {
+    manager.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
+  }
+
+  protected void fillLocalToolBar(IToolBarManager manager)
+  {
+    manager.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
+  }
+
+  protected final void showMessage(String message)
+  {
+    showMessage(MessageType.INFORMATION, message);
+  }
+
+  protected final boolean showMessage(MessageType type, String message)
+  {
+    switch (type)
+    {
+    case INFORMATION:
+      MessageDialog.openInformation(getShell(), getTitle(), message);
+      return true;
+
+    case ERROR:
+      MessageDialog.openError(getShell(), getTitle(), message);
+      return true;
+
+    case WARNING:
+      MessageDialog.openWarning(getShell(), getTitle(), message);
+      return true;
+
+    case CONFIRM:
+      return MessageDialog.openConfirm(getShell(), getTitle(), message);
+
+    case QUESTION:
+      return MessageDialog.openQuestion(getShell(), getTitle(), message);
+
+    default:
+      return true;
+    }
+  }
+
+  protected final Display getDisplay()
+  {
+    Display display = getCurrentViewer().getControl().getDisplay();
+    if (display == null)
+    {
+      display = UIUtil.getDisplay();
+    }
+
+    return display;
+  }
+
+  private void hookDoubleClick()
+  {
+    getCurrentViewer().addDoubleClickListener(new IDoubleClickListener()
+    {
+      public void doubleClick(DoubleClickEvent event)
+      {
+        IStructuredSelection selection = (IStructuredSelection)getCurrentViewer().getSelection();
+        Object object = selection.getFirstElement();
+        doubleClicked(object);
+      }
+    });
+  }
+
+  private void hookContextMenu(final StructuredViewer viewer)
+  {
+    MenuManager menuMgr = new MenuManager("#PopupMenu");
+    menuMgr.setRemoveAllWhenShown(true);
+    menuMgr.addMenuListener(new IMenuListener()
+    {
+      public void menuAboutToShow(IMenuManager manager)
+      {
+        IStructuredSelection selection = (IStructuredSelection)getCurrentViewer().getSelection();
+        fillContextMenu(manager, viewer, selection);
+      }
+    });
+
+    Control control = viewer.getControl();
+    Menu menu = menuMgr.createContextMenu(control);
+    viewer.getControl().setMenu(menu);
+    getSite().registerContextMenu(menuMgr, viewer);
+  }
+
+  private void contributeToActionBars()
+  {
+    IActionBars bars = getViewSite().getActionBars();
+    fillLocalPullDown(bars.getMenuManager());
+    fillLocalToolBar(bars.getToolBarManager());
+  }
+
+  protected static enum MessageType
+  {
+    INFORMATION, ERROR, WARNING, CONFIRM, QUESTION
+  }
+}

6dc01dbef3a155635d3b975cd6261431a60f88d5
Eike Stepper <stepper@esc-net.de>
2008-10-07 10:33:30 +0000

    [248902]  Can't set URI on CDOResource https://bugs.eclipse.org/bugs/show_bug.cgi?id=248902

--- plugins/org.eclipse.emf.cdo.tests/src/org/eclipse/emf/cdo/tests/ResourceTest.java
+++ plugins/org.eclipse.emf.cdo.tests/src/org/eclipse/emf/cdo/tests/ResourceTest.java
@@ -290,6 +290,48 @@ public class ResourceTest extends AbstractCDOTest
     session.close();
   }
 
+  public void testPathNotNull() throws Exception
+  {
+    {
+      CDOSession session = openModel1Session();
+      CDOTransaction transaction = session.openTransaction();
+      CDOResource resource = transaction.createResource("/res1");
+      assertEquals("/res1", resource.getPath());
+      assertEquals(CDOURIUtil.createResourceURI(session, "/res1"), resource.getURI());
+  
+      transaction.commit();
+      assertEquals("/res1", resource.getPath());
+      assertEquals(CDOURIUtil.createResourceURI(session, "/res1"), resource.getURI());
+      session.close();
+    }
+  
+    {
+      CDOSession session = openModel1Session();
+      CDOTransaction transaction = session.openTransaction();
+      CDOResource resource = transaction.getResource("/res1");
+      assertEquals("/res1", resource.getPath());
+      assertEquals(CDOURIUtil.createResourceURI(session, "/res1"), resource.getURI());
+  
+      CDOResource resource2 = transaction.getOrCreateResource("/res2");
+      assertEquals("/res2", resource2.getPath());
+      assertEquals(CDOURIUtil.createResourceURI(session, "/res2"), resource2.getURI());
+  
+      transaction.commit();
+      assertEquals("/res2", resource2.getPath());
+      assertEquals(CDOURIUtil.createResourceURI(session, "/res2"), resource2.getURI());
+      session.close();
+    }
+  
+    {
+      CDOSession session = openModel1Session();
+      CDOView view = session.openView();
+      CDOResource resource2 = view.getResource("/res2");
+      assertEquals("/res2", resource2.getPath());
+      assertEquals(CDOURIUtil.createResourceURI(session, "/res2"), resource2.getURI());
+      session.close();
+    }
+  }
+
   /**
    * http://bugs.eclipse.org/208689
    */

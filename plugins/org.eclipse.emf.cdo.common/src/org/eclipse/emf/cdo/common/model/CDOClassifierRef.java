176aef0032ab63b9a8e69ee7e961bdb93de2c30f
Eike Stepper <stepper@esc-net.de>
2009-05-07 15:39:11 +0000

    [247226]  Transparently support legacy models https://bugs.eclipse.org/bugs/show_bug.cgi?id=247226

--- plugins/org.eclipse.emf.cdo.common/src/org/eclipse/emf/cdo/common/model/CDOClassifierRef.java
+++ plugins/org.eclipse.emf.cdo.common/src/org/eclipse/emf/cdo/common/model/CDOClassifierRef.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *    Eike Stepper - initial API and implementation
  */
@@ -83,6 +83,6 @@ public final class CDOClassifierRef
   @Override
   public String toString()
   {
-    return MessageFormat.format("EClassRef({0}, {1})", packageURI, classifierID); //$NON-NLS-1$
+    return MessageFormat.format("CDOClassifierRef({0}, {1})", packageURI, classifierID); //$NON-NLS-1$
   }
 }

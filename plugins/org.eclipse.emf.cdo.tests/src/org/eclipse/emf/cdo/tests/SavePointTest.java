95e39a32bf81d4c46de4e4041d6a6bf1a2581473
Eike Stepper <stepper@esc-net.de>
2009-05-14 10:39:55 +0000

    example test case

--- plugins/org.eclipse.emf.cdo.tests/src/org/eclipse/emf/cdo/tests/SavePointTest.java
+++ plugins/org.eclipse.emf.cdo.tests/src/org/eclipse/emf/cdo/tests/SavePointTest.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *    Simon McDuff - initial API and implementation
  *    Eike Stepper - maintenance
@@ -19,11 +19,15 @@ import org.eclipse.emf.cdo.tests.model1.Company;
 import org.eclipse.emf.cdo.transaction.CDOSavepoint;
 import org.eclipse.emf.cdo.transaction.CDOTransaction;
 import org.eclipse.emf.cdo.util.CDOUtil;
+import org.eclipse.emf.cdo.view.CDOView;
 
 import org.eclipse.emf.internal.cdo.util.FSMUtil;
 
 /**
  * @author Simon McDuff
+ */
+/**
+ * @author Eike Stepper
  */
 public class SavePointTest extends AbstractCDOTest
 {
@@ -166,7 +170,7 @@ public class SavePointTest extends AbstractCDOTest
     assertEquals(false, transaction1.isDirty());
   }
 
-  public void flow1(boolean commitBegin, boolean commitEnd) throws Exception
+  private void flow1(boolean commitBegin, boolean commitEnd) throws Exception
   {
     CDOSession session = openModel1Session();
     session.getPackageRegistry().putEPackage(getModel1Package());
@@ -263,5 +267,69 @@ public class SavePointTest extends AbstractCDOTest
       assertEquals(commitBegin, !FSMUtil.isTransient(CDOUtil.getCDOObject(company1)));
       assertEquals(commitBegin, !FSMUtil.isTransient(resource1));
     }
+  }
+
+  /**
+   * Compare http://www.eclipse.org/newsportal/article.php?id=41697&group=eclipse.tools.emf#41697
+   * 
+   * <pre>
+   * Passive update is enabled.
+   * client1 sets a save point
+   * client1 write locks object1
+   * client1 modifies object1
+   * client2 modifies object2
+   * client2 commits
+   * client1 rolls back to save point
+   * Result:
+   * Repository: object1 not modified, object2 is modified
+   * client1: same as repository
+   * client2: same as repository
+   *</pre>
+   */
+  public void testScenario1()
+  {
+    CDOSession client1 = openSession();
+    CDOTransaction transaction1 = client1.openTransaction();
+    CDOResource object1X = transaction1.createResource("/object1");
+    CDOResource object2X = transaction1.createResource("/object2");
+    transaction1.commit();
+
+    // client1 sets a save point
+    CDOSavepoint savepoint = transaction1.setSavepoint();
+
+    // client1 write locks object1
+    object1X.cdoWriteLock().lock();
+
+    // client1 modifies object1
+    object1X.getContents().add(getModel1Factory().createCompany());
+
+    // client2 modifies object2
+    CDOSession client2 = openSession();
+    CDOTransaction transaction2 = client2.openTransaction();
+    CDOResource object2Y = transaction2.getResource("/object2");
+    object2Y.getContents().add(getModel1Factory().createOrder());
+
+    // client2 commits
+    transaction2.commit();
+
+    // client1 rolls back to save point
+    savepoint.rollback();
+
+    // Repository: object1 not modified, object2 is modified
+    CDOSession client3 = openSession();
+    CDOView view = client3.openView();
+    CDOResource object1Test = view.getResource("/object1");
+    assertEquals(0, object1Test.getContents().size());
+    CDOResource object2Test = view.getResource("/object2");
+    assertEquals(1, object2Test.getContents().size());
+
+    // client1: same as repository
+    assertEquals(object1Test.getContents().size(), object1X.getContents().size());
+    assertEquals(object2Test.getContents().size(), object2X.getContents().size());
+
+    // client2: same as repository
+    CDOResource object1Y = transaction2.getResource("/object1");
+    assertEquals(object1Test.getContents().size(), object1Y.getContents().size());
+    assertEquals(object2Test.getContents().size(), object2Y.getContents().size());
   }
 }

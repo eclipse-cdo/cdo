64ac27512a879b4598b8b748e7f3bd1d566cf099
Eike Stepper <stepper@esc-net.de>
2009-05-19 15:37:09 +0000

    leaner test case

--- plugins/org.eclipse.net4j.tests/src/org/eclipse/net4j/tests/SignalTest.java
+++ plugins/org.eclipse.net4j.tests/src/org/eclipse/net4j/tests/SignalTest.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *    Eike Stepper - initial API and implementation
  */
@@ -81,7 +81,7 @@ public class SignalTest extends AbstractProtocolTest
       OMPlatform.INSTANCE.setDebugging(false);
       protocol = new TestSignalProtocol(connector);
       String data = TinyData.getText();
-      for (int i = 0; i < 10000; i++)
+      for (int i = 0; i < 1000; i++)
       {
         msg("Loop " + i);
         new AsyncRequest(protocol, data).sendAsync();

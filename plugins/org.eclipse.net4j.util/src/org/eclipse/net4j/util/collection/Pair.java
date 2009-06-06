54c067e635caa8deb8549f382be7e2edb0c692e4
Eike Stepper <stepper@esc-net.de>
2009-06-06 21:01:56 +0000

    copy ctor

--- plugins/org.eclipse.net4j.util/src/org/eclipse/net4j/util/collection/Pair.java
+++ plugins/org.eclipse.net4j.util/src/org/eclipse/net4j/util/collection/Pair.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *    Eike Stepper - initial API and implementation
  */
@@ -29,6 +29,15 @@ public class Pair<T1, T2>
   {
     this.element1 = element1;
     this.element2 = element2;
+  }
+
+  /**
+   * @since 2.0
+   */
+  public Pair(Pair<T1, T2> source)
+  {
+    element1 = source.element1;
+    element2 = source.element2;
   }
 
   public final T1 getElement1()

4eb7d3d21b821d015a25bb62814f9e711b9feae8
Eike Stepper <stepper@esc-net.de>
2009-06-12 09:37:34 +0000

    [276845] Develop A CDORevisionCache that uses the file system https://bugs.eclipse.org/bugs/show_bug.cgi?id=276845

--- plugins/org.eclipse.emf.cdo.common.db/src/org/eclipse/emf/cdo/common/internal/db/DBRevisionCacheUtil.java
+++ plugins/org.eclipse.emf.cdo.common.db/src/org/eclipse/emf/cdo/common/internal/db/DBRevisionCacheUtil.java
@@ -11,56 +11,54 @@
  **************************************************************************/
 package org.eclipse.emf.cdo.common.internal.db;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.text.MessageFormat;
-
 import org.eclipse.emf.cdo.common.model.CDOModelConstants;
 import org.eclipse.emf.cdo.common.revision.CDORevision;
 import org.eclipse.emf.cdo.spi.common.revision.InternalCDORevision;
-import org.eclipse.emf.ecore.EStructuralFeature;
+
 import org.eclipse.net4j.db.DBException;
 import org.eclipse.net4j.util.CheckUtil;
+
+import org.eclipse.emf.ecore.EStructuralFeature;
 
 /**
  * @author Andre Dietisheim
  */
-public class DBRevisionCacheUtil {
+public class DBRevisionCacheUtil
+{
 
-	/**
-	 * Asserts the given {@link CDORevision} is <tt>null</tt>.
-	 * 
-	 * @param cdoRevision
-	 *            the cdo revision
-	 * @param message
-	 *            the message to use when throwing the {@link DBException}
-	 * @throws DBException
-	 *             if the given CDORevision's not <tt>null</tt>
-	 */
-	public static void assertIsNull(CDORevision cdoRevision, String message) {
-		if (cdoRevision != null) {
-			throw new DBException(message);
-		}
-	}
+  /**
+   * Asserts the given {@link CDORevision} is <tt>null</tt>.
+   * 
+   * @param cdoRevision
+   *          the cdo revision
+   * @param message
+   *          the message to use when throwing the {@link DBException}
+   * @throws DBException
+   *           if the given CDORevision's not <tt>null</tt>
+   */
+  public static void assertIsNull(CDORevision cdoRevision, String message)
+  {
+    if (cdoRevision != null)
+    {
+      throw new DBException(message);
+    }
+  }
 
-	/**
-	 * Gets the name of a revision of a CDOResourceNode.
-	 * 
-	 * @param revision
-	 *            the revision
-	 * 
-	 * @return the resource node name
-	 */
-	// TODO: this should be refactored and put in a place, that's more generic
-	// than this class. The same snippet's used in LRURevisionCache and
-	// MemRevisionCache
-	public static String getResourceNodeName(CDORevision revision) {
-		CheckUtil.checkArg(revision.isResourceNode(),
-				"the revision is not a resource node!");
-		EStructuralFeature feature = revision.getEClass()
-				.getEStructuralFeature(
-						CDOModelConstants.RESOURCE_NODE_NAME_ATTRIBUTE);
-		return (String) ((InternalCDORevision) revision).getValue(feature);
-	}
+  /**
+   * Gets the name of a revision of a CDOResourceNode.
+   * 
+   * @param revision
+   *          the revision
+   * @return the resource node name
+   */
+  // TODO: this should be refactored and put in a place, that's more generic
+  // than this class. The same snippet's used in LRURevisionCache and
+  // MemRevisionCache
+  public static String getResourceNodeName(CDORevision revision)
+  {
+    CheckUtil.checkArg(revision.isResourceNode(), "the revision is not a resource node!");
+    EStructuralFeature feature = revision.getEClass().getEStructuralFeature(
+        CDOModelConstants.RESOURCE_NODE_NAME_ATTRIBUTE);
+    return (String)((InternalCDORevision)revision).getValue(feature);
+  }
 }
--- plugins/org.eclipse.emf.cdo.common.db/src/org/eclipse/emf/cdo/common/internal/db/cache/DBRevisionCacheSchema.java
+++ plugins/org.eclipse.emf.cdo.common.db/src/org/eclipse/emf/cdo/common/internal/db/cache/DBRevisionCacheSchema.java
@@ -20,57 +20,58 @@ import org.eclipse.net4j.spi.db.DBSchema;
 /**
  * @author Andre Dietisheim
  */
-public class DBRevisionCacheSchema extends DBSchema {
-	public static final DBRevisionCacheSchema INSTANCE = new DBRevisionCacheSchema();
+public class DBRevisionCacheSchema extends DBSchema
+{
+  public static final DBRevisionCacheSchema INSTANCE = new DBRevisionCacheSchema();
 
-	/**
-	 * DBTable dbrevisioncache_revisions
-	 */
-	public static final IDBTable REVISIONS = INSTANCE
-			.addTable("dbrevisioncache_revisions");
+  /**
+   * DBTable dbrevisioncache_revisions
+   */
+  public static final IDBTable REVISIONS = INSTANCE.addTable("dbrevisioncache_revisions");
 
-	public static final IDBField REVISIONS_ID = //
-	REVISIONS.addField("id", DBType.BIGINT);
+  public static final IDBField REVISIONS_ID = //
+  REVISIONS.addField("id", DBType.BIGINT);
 
-	public static final IDBIndex INDEX_REVISIONS_ID = //
-	REVISIONS.addIndex(IDBIndex.Type.NON_UNIQUE, REVISIONS_ID);
+  public static final IDBIndex INDEX_REVISIONS_ID = //
+  REVISIONS.addIndex(IDBIndex.Type.NON_UNIQUE, REVISIONS_ID);
 
-	public static final IDBField REVISIONS_VERSION = //
-	REVISIONS.addField("version", DBType.BIGINT);
+  public static final IDBField REVISIONS_VERSION = //
+  REVISIONS.addField("version", DBType.BIGINT);
 
-	public static final IDBIndex INDEX_REVISIONS_VERSION = //
-	REVISIONS.addIndex(IDBIndex.Type.NON_UNIQUE, REVISIONS_VERSION);
+  public static final IDBIndex INDEX_REVISIONS_VERSION = //
+  REVISIONS.addIndex(IDBIndex.Type.NON_UNIQUE, REVISIONS_VERSION);
 
-	public static final IDBIndex INDEX_REVISIONS_PK = //
-	REVISIONS.addIndex(IDBIndex.Type.PRIMARY_KEY, REVISIONS_ID,
-			REVISIONS_VERSION);
+  public static final IDBIndex INDEX_REVISIONS_PK = //
+  REVISIONS.addIndex(IDBIndex.Type.PRIMARY_KEY, REVISIONS_ID, REVISIONS_VERSION);
 
-	public static final IDBField REVISIONS_CREATED = //
-	REVISIONS.addField("created", DBType.BIGINT);
+  public static final IDBField REVISIONS_CREATED = //
+  REVISIONS.addField("created", DBType.BIGINT);
 
-	public static final IDBField REVISIONS_REVISED = //
-	REVISIONS.addField("revised", DBType.BIGINT);
+  public static final IDBField REVISIONS_REVISED = //
+  REVISIONS.addField("revised", DBType.BIGINT);
 
-	public static final IDBField REVISIONS_CDOREVISION = //
-	REVISIONS.addField("cdorevision", DBType.BLOB);
+  public static final IDBField REVISIONS_CDOREVISION = //
+  REVISIONS.addField("cdorevision", DBType.BLOB);
 
-	public static final IDBField REVISIONS_RESOURCENODENAME = //
-	REVISIONS.addField("resourcenode_name", DBType.VARCHAR, false);
+  public static final IDBField REVISIONS_RESOURCENODENAME = //
+  REVISIONS.addField("resourcenode_name", DBType.VARCHAR, false);
 
-	public static final IDBIndex INDEX_REVISIONS_RESOURCENODENAME = //
-	REVISIONS.addIndex(IDBIndex.Type.NON_UNIQUE, REVISIONS_RESOURCENODENAME);
+  public static final IDBIndex INDEX_REVISIONS_RESOURCENODENAME = //
+  REVISIONS.addIndex(IDBIndex.Type.NON_UNIQUE, REVISIONS_RESOURCENODENAME);
 
-	public static final IDBField REVISIONS_CONTAINERID = //
-	REVISIONS.addField("container_id", DBType.BIGINT, false);
+  public static final IDBField REVISIONS_CONTAINERID = //
+  REVISIONS.addField("container_id", DBType.BIGINT, false);
 
-	public static final IDBIndex INDEX_REVISIONS_CONTAINERID = //
-	REVISIONS.addIndex(IDBIndex.Type.NON_UNIQUE, REVISIONS_CONTAINERID);
+  public static final IDBIndex INDEX_REVISIONS_CONTAINERID = //
+  REVISIONS.addIndex(IDBIndex.Type.NON_UNIQUE, REVISIONS_CONTAINERID);
 
-	private DBRevisionCacheSchema() {
-		super("dbRevisionCache");
-	}
+  private DBRevisionCacheSchema()
+  {
+    super("dbRevisionCache");
+  }
 
-	static {
-		INSTANCE.lock();
-	}
+  static
+  {
+    INSTANCE.lock();
+  }
 }

ba8c15fc74e22077bf86f20dc74fbef9fea7cdb4
Eike Stepper <stepper@esc-net.de>
2009-07-04 04:56:50 +0000

    test

--- plugins/org.eclipse.net4j.tests/src/org/eclipse/net4j/util/tests/SynchronizingCorrelatorTest.java
+++ plugins/org.eclipse.net4j.tests/src/org/eclipse/net4j/util/tests/SynchronizingCorrelatorTest.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *    Eike Stepper - initial API and implementation
  */
@@ -34,7 +34,7 @@ public class SynchronizingCorrelatorTest extends AbstractOMTest
     };
 
     consumer.start();
-    sleep(50);
+    sleep(100);
 
     correlator.put("eike", true); //$NON-NLS-1$
     consumer.join(1000);

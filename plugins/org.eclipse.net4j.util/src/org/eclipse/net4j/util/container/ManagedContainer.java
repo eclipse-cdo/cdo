9c3c98995db8303ce9184d05cd4f9bcb2595f01a
Eike Stepper <stepper@esc-net.de>
2009-07-05 06:09:10 +0000

    clear container elements in deactivate

--- plugins/org.eclipse.net4j.util/src/org/eclipse/net4j/util/container/ManagedContainer.java
+++ plugins/org.eclipse.net4j.util/src/org/eclipse/net4j/util/container/ManagedContainer.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *    Eike Stepper - initial API and implementation
  */
@@ -539,7 +539,13 @@ public class ManagedContainer extends Lifecycle implements IManagedContainer
     }
 
     LifecycleUtil.deactivate(factoryRegistry);
+    factoryRegistry = null;
+
     LifecycleUtil.deactivate(postProcessors);
+    postProcessors = null;
+
+    elementRegistry.clear();
+    elementRegistry = null;
     super.doDeactivate();
   }
 

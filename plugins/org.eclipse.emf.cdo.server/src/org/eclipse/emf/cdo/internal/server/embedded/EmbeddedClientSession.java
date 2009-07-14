ad8f8b1ad4d657dfa141ec9d9a0003b6af70a610
Eike Stepper <stepper@esc-net.de>
2009-07-14 16:07:04 +0000

    [282481]  Provide an EmbeddedSessionProtocol https://bugs.eclipse.org/bugs/show_bug.cgi?id=282481

--- plugins/org.eclipse.emf.cdo.server/src/org/eclipse/emf/cdo/internal/server/embedded/EmbeddedClientSession.java
+++ plugins/org.eclipse.emf.cdo.server/src/org/eclipse/emf/cdo/internal/server/embedded/EmbeddedClientSession.java
@@ -11,11 +11,22 @@
 package org.eclipse.emf.cdo.internal.server.embedded;
 
 import org.eclipse.emf.cdo.server.embedded.CDOSession;
+import org.eclipse.emf.cdo.spi.common.model.InternalCDOPackageInfo;
 import org.eclipse.emf.cdo.spi.common.model.InternalCDOPackageRegistry;
+import org.eclipse.emf.cdo.spi.common.model.InternalCDOPackageUnit;
 import org.eclipse.emf.cdo.spi.common.revision.InternalCDORevisionManager;
 import org.eclipse.emf.cdo.spi.server.InternalRepository;
 
 import org.eclipse.emf.internal.cdo.session.CDOSessionImpl;
+
+import org.eclipse.net4j.util.lifecycle.Lifecycle;
+
+import org.eclipse.emf.ecore.EFactory;
+import org.eclipse.emf.ecore.EPackage;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * @author Eike Stepper
@@ -46,5 +57,165 @@ public class EmbeddedClientSession extends CDOSessionImpl implements CDOSession
   public InternalCDORevisionManager getRevisionManager()
   {
     return getRepository().getRevisionManager();
+  }
+
+  /**
+   * @author Eike Stepper
+   */
+  private class PackageRegistry extends Lifecycle implements InternalCDOPackageRegistry
+  {
+    public PackageRegistry()
+    {
+    }
+
+    public InternalCDOPackageRegistry getDelegate()
+    {
+      return getRepository().getPackageRegistry();
+    }
+
+    public Object basicPut(String nsURI, Object value)
+    {
+      return getDelegate().basicPut(nsURI, value);
+    }
+
+    public void clear()
+    {
+      getDelegate().clear();
+    }
+
+    public boolean containsKey(Object key)
+    {
+      return getDelegate().containsKey(key);
+    }
+
+    public boolean containsValue(Object value)
+    {
+      return getDelegate().containsValue(value);
+    }
+
+    public Set<java.util.Map.Entry<String, Object>> entrySet()
+    {
+      return getDelegate().entrySet();
+    }
+
+    public Object get(Object key)
+    {
+      return getDelegate().get(key);
+    }
+
+    public EFactory getEFactory(String nsURI)
+    {
+      return getDelegate().getEFactory(nsURI);
+    }
+
+    public EPackage getEPackage(String nsURI)
+    {
+      return getDelegate().getEPackage(nsURI);
+    }
+
+    public EPackage[] getEPackages()
+    {
+      return getDelegate().getEPackages();
+    }
+
+    public MetaInstanceMapper getMetaInstanceMapper()
+    {
+      return getDelegate().getMetaInstanceMapper();
+    }
+
+    public InternalCDOPackageInfo getPackageInfo(EPackage ePackage)
+    {
+      return getDelegate().getPackageInfo(ePackage);
+    }
+
+    public InternalCDOPackageInfo[] getPackageInfos()
+    {
+      return getDelegate().getPackageInfos();
+    }
+
+    public PackageLoader getPackageLoader()
+    {
+      return getDelegate().getPackageLoader();
+    }
+
+    public PackageProcessor getPackageProcessor()
+    {
+      return getDelegate().getPackageProcessor();
+    }
+
+    public InternalCDOPackageUnit getPackageUnit(EPackage ePackage)
+    {
+      return getDelegate().getPackageUnit(ePackage);
+    }
+
+    public InternalCDOPackageUnit[] getPackageUnits()
+    {
+      return getDelegate().getPackageUnits();
+    }
+
+    public boolean isEmpty()
+    {
+      return getDelegate().isEmpty();
+    }
+
+    public boolean isReplacingDescriptors()
+    {
+      return getDelegate().isReplacingDescriptors();
+    }
+
+    public Set<String> keySet()
+    {
+      return getDelegate().keySet();
+    }
+
+    public Object put(String key, Object value)
+    {
+      return getDelegate().put(key, value);
+    }
+
+    public void putAll(Map<? extends String, ? extends Object> t)
+    {
+      getDelegate().putAll(t);
+    }
+
+    public Object putEPackage(EPackage ePackage)
+    {
+      return getDelegate().putEPackage(ePackage);
+    }
+
+    public void putPackageUnit(InternalCDOPackageUnit packageUnit)
+    {
+      getDelegate().putPackageUnit(packageUnit);
+    }
+
+    public Object remove(Object key)
+    {
+      return getDelegate().remove(key);
+    }
+
+    public void setPackageLoader(PackageLoader packageLoader)
+    {
+      getDelegate().setPackageLoader(packageLoader);
+    }
+
+    public void setPackageProcessor(PackageProcessor packageProcessor)
+    {
+      getDelegate().setPackageProcessor(packageProcessor);
+    }
+
+    public void setReplacingDescriptors(boolean replacingDescriptors)
+    {
+      getDelegate().setReplacingDescriptors(replacingDescriptors);
+    }
+
+    public int size()
+    {
+      return getDelegate().size();
+    }
+
+    public Collection<Object> values()
+    {
+      return getDelegate().values();
+    }
   }
 }

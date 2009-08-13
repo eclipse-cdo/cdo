b69e8d66a258dbcee2f41067304935ba57f85426
Eike Stepper <stepper@esc-net.de>
2009-08-13 10:36:15 +0000

    narrow product type

--- plugins/org.eclipse.emf.cdo.server/src/org/eclipse/emf/cdo/spi/server/QueryHandlerFactory.java
+++ plugins/org.eclipse.emf.cdo.server/src/org/eclipse/emf/cdo/spi/server/QueryHandlerFactory.java
@@ -4,13 +4,16 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *    Eike Stepper - initial API and implementation
  */
 package org.eclipse.emf.cdo.spi.server;
 
+import org.eclipse.emf.cdo.server.IQueryHandler;
+
 import org.eclipse.net4j.util.factory.Factory;
+import org.eclipse.net4j.util.factory.ProductCreationException;
 
 /**
  * @author Eike Stepper
@@ -24,4 +27,9 @@ public abstract class QueryHandlerFactory extends Factory
   {
     super(PRODUCT_GROUP, language);
   }
+
+  /**
+   * @since 3.0
+   */
+  public abstract IQueryHandler create(String description) throws ProductCreationException;
 }

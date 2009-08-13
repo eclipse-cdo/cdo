95dc6d939d535841c6ce653ab753fac6a509de79
Andre Dietisheim <andre.dietisheim@gmail.com>
2009-08-13 16:07:12 +0000

    CDORevisionCacheTests now extend AbstractOMTest

--- plugins/org.eclipse.emf.cdo.tests/src/org/eclipse/emf/cdo/tests/revisioncache/AbstractCDORevisionCacheTest.java
+++ plugins/org.eclipse.emf.cdo.tests/src/org/eclipse/emf/cdo/tests/revisioncache/AbstractCDORevisionCacheTest.java
@@ -17,16 +17,32 @@ import org.eclipse.emf.cdo.common.id.CDOIDUtil;
 import org.eclipse.emf.cdo.common.revision.CDORevision;
 import org.eclipse.emf.cdo.common.revision.cache.CDORevisionCache;
 import org.eclipse.emf.cdo.eresource.CDOResource;
+import org.eclipse.emf.cdo.net4j.CDONet4jUtil;
+import org.eclipse.emf.cdo.net4j.CDOSessionConfiguration;
+import org.eclipse.emf.cdo.server.CDOServerUtil;
+import org.eclipse.emf.cdo.server.IMEMStore;
+import org.eclipse.emf.cdo.server.IRepository;
+import org.eclipse.emf.cdo.server.mem.MEMStoreUtil;
+import org.eclipse.emf.cdo.server.net4j.CDONet4jServerUtil;
 import org.eclipse.emf.cdo.session.CDOSession;
 import org.eclipse.emf.cdo.spi.common.revision.InternalCDORevision;
-import org.eclipse.emf.cdo.tests.AbstractCDOTest;
+import org.eclipse.emf.cdo.tests.model1.Model1Factory;
+import org.eclipse.emf.cdo.tests.model1.Model1Package;
 import org.eclipse.emf.cdo.tests.model1.impl.AddressImpl;
 import org.eclipse.emf.cdo.tests.model1.impl.CompanyImpl;
 import org.eclipse.emf.cdo.transaction.CDOTransaction;
 
+import org.eclipse.net4j.Net4jUtil;
+import org.eclipse.net4j.jvm.IJVMAcceptor;
+import org.eclipse.net4j.jvm.IJVMConnector;
+import org.eclipse.net4j.jvm.JVMUtil;
+import org.eclipse.net4j.util.CheckUtil;
+import org.eclipse.net4j.util.container.ContainerUtil;
+import org.eclipse.net4j.util.container.IManagedContainer;
+import org.eclipse.net4j.util.lifecycle.Lifecycle;
 import org.eclipse.net4j.util.lifecycle.LifecycleUtil;
+import org.eclipse.net4j.util.tests.AbstractOMTest;
 
-import org.eclipse.emf.spi.cdo.InternalCDOSession;
 import org.eclipse.emf.spi.cdo.InternalCDOTransaction;
 
 import java.util.List;
@@ -34,24 +50,39 @@ import java.util.List;
 /**
  * @author Andre Dietisheim
  */
-public abstract class AbstractCDORevisionCacheTest extends AbstractCDOTest
+public abstract class AbstractCDORevisionCacheTest extends AbstractOMTest
 {
+
   private static final String RESOURCE_PATH = "/res1";
 
   private CDOResource resource;
 
   private CDORevisionCache revisionCache;
 
+  private SessionFactory sessionFactory;
+
   @Override
   protected void doSetUp() throws Exception
   {
-    skipUnlessConfig(MEM);
     super.doSetUp();
 
-    InternalCDOSession session = (InternalCDOSession)openSession();
+    sessionFactory = new SessionFactory();
+    LifecycleUtil.activate(sessionFactory);
+    CDOSession session = sessionFactory.openSession();
+    session.getPackageRegistry().putEPackage(Model1Package.eINSTANCE);
+
     resource = createResource(session);
     revisionCache = createRevisionCache(session);
     LifecycleUtil.activate(revisionCache);
+  }
+
+  @Override
+  protected void doTearDown() throws Exception
+  {
+    LifecycleUtil.deactivate(sessionFactory);
+    LifecycleUtil.deactivate(revisionCache);
+    LifecycleUtil.deactivate(resource.cdoView().getSession());
+    super.doTearDown();
   }
 
   private CDOResource createResource(CDOSession session)
@@ -60,18 +91,10 @@ public abstract class AbstractCDORevisionCacheTest extends AbstractCDOTest
     return transaction.createResource(RESOURCE_PATH);
   }
 
-  @Override
-  protected void doTearDown() throws Exception
-  {
-    LifecycleUtil.deactivate(revisionCache);
-    LifecycleUtil.deactivate(resource.cdoView().getSession());
-    super.doTearDown();
-  }
-
   public void testAddedRevisionIsGettable()
   {
     CDOTransaction transaction = (CDOTransaction)resource.cdoView();
-    CompanyImpl company = (CompanyImpl)getModel1Factory().createCompany();
+    CompanyImpl company = (CompanyImpl)Model1Factory.eINSTANCE.createCompany();
     company.setName("Puzzle");
     resource.getContents().add(company);
     transaction.commit();
@@ -86,7 +109,7 @@ public abstract class AbstractCDORevisionCacheTest extends AbstractCDOTest
   public void testGetRevisionReturnsLatestVersion()
   {
     CDOTransaction transaction = (CDOTransaction)resource.cdoView();
-    CompanyImpl company = (CompanyImpl)getModel1Factory().createCompany();
+    CompanyImpl company = (CompanyImpl)Model1Factory.eINSTANCE.createCompany();
     company.setName("Puzzle");
     resource.getContents().add(company);
     transaction.commit();
@@ -105,7 +128,7 @@ public abstract class AbstractCDORevisionCacheTest extends AbstractCDOTest
   public void testAddedRevisionIsNotRevised()
   {
     CDOTransaction transaction = (CDOTransaction)resource.cdoView();
-    CompanyImpl company = (CompanyImpl)getModel1Factory().createCompany();
+    CompanyImpl company = (CompanyImpl)Model1Factory.eINSTANCE.createCompany();
     company.setName("Puzzle");
     resource.getContents().add(company);
     transaction.commit();
@@ -120,7 +143,7 @@ public abstract class AbstractCDORevisionCacheTest extends AbstractCDOTest
   public void testFormerVersionIsGettable()
   {
     CDOTransaction transaction = (CDOTransaction)resource.cdoView();
-    CompanyImpl company = (CompanyImpl)getModel1Factory().createCompany();
+    CompanyImpl company = (CompanyImpl)Model1Factory.eINSTANCE.createCompany();
     company.setName("Puzzle");
     resource.getContents().add(company);
     transaction.commit();
@@ -144,7 +167,7 @@ public abstract class AbstractCDORevisionCacheTest extends AbstractCDOTest
   public void testAddRevisionUpdatesRevisedTimeStampOfLastRevision()
   {
     CDOTransaction transaction = (CDOTransaction)resource.cdoView();
-    CompanyImpl company = (CompanyImpl)getModel1Factory().createCompany();
+    CompanyImpl company = (CompanyImpl)Model1Factory.eINSTANCE.createCompany();
     company.setName("Puzzle");
     resource.getContents().add(company);
     transaction.commit();
@@ -172,7 +195,7 @@ public abstract class AbstractCDORevisionCacheTest extends AbstractCDOTest
   public void testTheFormerRevisionOf2VersionsMayBeFetchedByTimestamp()
   {
     CDOTransaction transaction = (CDOTransaction)resource.cdoView();
-    CompanyImpl company = (CompanyImpl)getModel1Factory().createCompany();
+    CompanyImpl company = (CompanyImpl)Model1Factory.eINSTANCE.createCompany();
     company.setName("Puzzle");
     resource.getContents().add(company);
     transaction.commit();
@@ -201,7 +224,7 @@ public abstract class AbstractCDORevisionCacheTest extends AbstractCDOTest
   public void testGiven3ObjectsOf2TypesGetRevisionsReturns2Versions()
   {
     CDOTransaction transaction = (CDOTransaction)resource.cdoView();
-    CompanyImpl company = (CompanyImpl)getModel1Factory().createCompany();
+    CompanyImpl company = (CompanyImpl)Model1Factory.eINSTANCE.createCompany();
     resource.getContents().add(company);
 
     company.setName("Puzzle");
@@ -212,7 +235,7 @@ public abstract class AbstractCDORevisionCacheTest extends AbstractCDOTest
     transaction.commit();
     revisionCache.addRevision(company.cdoRevision());
 
-    AddressImpl address = (AddressImpl)getModel1Factory().createAddress();
+    AddressImpl address = (AddressImpl)Model1Factory.eINSTANCE.createAddress();
     address.setStreet("Eigerplatz 4");
     resource.getContents().add(address);
     transaction.commit();
@@ -226,7 +249,7 @@ public abstract class AbstractCDORevisionCacheTest extends AbstractCDOTest
   public void testReturnsRemovedVersionWhenRemoving()
   {
     CDOTransaction transaction = (CDOTransaction)resource.cdoView();
-    CompanyImpl company = (CompanyImpl)getModel1Factory().createCompany();
+    CompanyImpl company = (CompanyImpl)Model1Factory.eINSTANCE.createCompany();
     resource.getContents().add(company);
 
     company.setName("Puzzle");
@@ -247,7 +270,7 @@ public abstract class AbstractCDORevisionCacheTest extends AbstractCDOTest
   public void testRemovedRevisionIsRemovedFromCache()
   {
     CDOTransaction transaction = (CDOTransaction)resource.cdoView();
-    CompanyImpl company = (CompanyImpl)getModel1Factory().createCompany();
+    CompanyImpl company = (CompanyImpl)Model1Factory.eINSTANCE.createCompany();
     resource.getContents().add(company);
 
     company.setName("Puzzle");
@@ -267,7 +290,7 @@ public abstract class AbstractCDORevisionCacheTest extends AbstractCDOTest
   public void testRemoveSecondRevisionResultsInNoActiveRevision()
   {
     CDOTransaction transaction = (CDOTransaction)resource.cdoView();
-    CompanyImpl company = (CompanyImpl)getModel1Factory().createCompany();
+    CompanyImpl company = (CompanyImpl)Model1Factory.eINSTANCE.createCompany();
     resource.getContents().add(company);
 
     company.setName("Puzzle");
@@ -288,7 +311,7 @@ public abstract class AbstractCDORevisionCacheTest extends AbstractCDOTest
   public void testRemovedRevisionIsNotGettableByTimeStamp()
   {
     CDOTransaction transaction = (CDOTransaction)resource.cdoView();
-    CompanyImpl company = (CompanyImpl)getModel1Factory().createCompany();
+    CompanyImpl company = (CompanyImpl)Model1Factory.eINSTANCE.createCompany();
     resource.getContents().add(company);
 
     company.setName("Puzzle");
@@ -309,7 +332,7 @@ public abstract class AbstractCDORevisionCacheTest extends AbstractCDOTest
   public void testClearedCacheDoesNotContainAnyRevisions()
   {
     CDOTransaction transaction = (CDOTransaction)resource.cdoView();
-    CompanyImpl company = (CompanyImpl)getModel1Factory().createCompany();
+    CompanyImpl company = (CompanyImpl)Model1Factory.eINSTANCE.createCompany();
     resource.getContents().add(company);
 
     company.setName("Puzzle");
@@ -337,11 +360,6 @@ public abstract class AbstractCDORevisionCacheTest extends AbstractCDOTest
     assertEquals(thisRevision.getRevised(), thatRevision.getRevised());
   }
 
-  protected CDOResource getResource()
-  {
-    return resource;
-  }
-
   @SuppressWarnings("unused")
   private boolean isTestFor(String testName)
   {
@@ -350,5 +368,93 @@ public abstract class AbstractCDORevisionCacheTest extends AbstractCDOTest
   }
 
   protected abstract CDORevisionCache createRevisionCache(CDOSession session) throws Exception;
+
+  public class SessionFactory extends Lifecycle
+  {
+    private static final String CONNECTOR_NAME = "server1";
+
+    private static final String REPO_NAME = "repo1";
+
+    private CDOSession session;
+
+    private IManagedContainer serverContainer;
+
+    private IJVMAcceptor acceptor;
+
+    private IMEMStore store;
+
+    private IRepository repository;
+
+    private IManagedContainer clientContainer;
+
+    private IJVMConnector connector;
+
+    private CDOSessionConfiguration configuration;
+
+    SessionFactory()
+    {
+    }
+
+    @Override
+    protected void doActivate() throws Exception
+    {
+      super.doActivate();
+      createRepository(REPO_NAME);
+      configuration = prepareSession();
+    }
+
+    @Override
+    protected void doDeactivate() throws Exception
+    {
+      super.doDeactivate();
+      LifecycleUtil.deactivate(session);
+      LifecycleUtil.deactivate(connector);
+      LifecycleUtil.deactivate(repository);
+      LifecycleUtil.deactivate(acceptor);
+      LifecycleUtil.deactivate(clientContainer);
+      LifecycleUtil.deactivate(serverContainer);
+    }
+
+    protected CDOSession openSession()
+    {
+      CheckUtil.checkNull(configuration, "session configuration is null, factory is not activated.");
+      return configuration.openSession();
+    }
+
+    private CDOSessionConfiguration prepareSession()
+    {
+      clientContainer = ContainerUtil.createContainer();
+      Net4jUtil.prepareContainer(clientContainer);
+      JVMUtil.prepareContainer(clientContainer);
+      CDONet4jUtil.prepareContainer(clientContainer);
+      LifecycleUtil.activate(clientContainer);
+
+      // Create configuration
+      CDOSessionConfiguration configuration = CDONet4jUtil.createSessionConfiguration();
+      connector = JVMUtil.getConnector(clientContainer, CONNECTOR_NAME);
+      configuration.setConnector(connector);
+      configuration.setRepositoryName(REPO_NAME);
+      return configuration;
+    }
+
+    protected void createRepository(String repositoryName)
+    {
+      serverContainer = ContainerUtil.createContainer();
+      Net4jUtil.prepareContainer(serverContainer); // Register Net4j factories
+      JVMUtil.prepareContainer(serverContainer);
+      CDONet4jServerUtil.prepareContainer(serverContainer);
+      LifecycleUtil.activate(serverContainer);
+
+      acceptor = JVMUtil.getAcceptor(serverContainer, CONNECTOR_NAME);
+      store = MEMStoreUtil.createMEMStore();
+      repository = CDOServerUtil.createRepository(repositoryName, store, null);
+      CDOServerUtil.addRepository(serverContainer, repository);
+    }
+
+    public CDOSession getSession()
+    {
+      return session;
+    }
+  }
 
 }

7eb982d7e92ad124c432491d62637620c3d6e243
Eike Stepper <stepper@esc-net.de>
2009-10-07 11:16:51 +0000

    javadoc

--- plugins/org.eclipse.emf.cdo/src/org/eclipse/emf/internal/cdo/view/CDOViewSetImpl.java
+++ plugins/org.eclipse.emf.cdo/src/org/eclipse/emf/internal/cdo/view/CDOViewSetImpl.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *    Simon McDuff - initial API and implementation
  *    Eike Stepper - maintenance
@@ -210,5 +210,7 @@ public class CDOViewSetImpl extends NotifierImpl implements InternalCDOViewSet
 
   public void notifyChanged(Notification notification)
   {
+    // Do nothing
+    // The resource <-> view association is done in CDOResourceImpl.basicSetResourceSet()
   }
 }

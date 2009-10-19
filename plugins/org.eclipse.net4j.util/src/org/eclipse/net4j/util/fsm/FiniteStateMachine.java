26cc83aae99d8d9ee94b058931b226475e350a43
Eike Stepper <stepper@esc-net.de>
2009-10-19 06:58:32 +0000

    [277059]  CDO element load/state_change notification https://bugs.eclipse.org/bugs/show_bug.cgi?id=277059

--- plugins/org.eclipse.net4j.util/src/org/eclipse/net4j/util/fsm/FiniteStateMachine.java
+++ plugins/org.eclipse.net4j.util/src/org/eclipse/net4j/util/fsm/FiniteStateMachine.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *    Eike Stepper - initial API and implementation
  */
@@ -196,9 +196,9 @@ public abstract class FiniteStateMachine<STATE extends Enum<?>, EVENT extends En
   protected void changeState(SUBJECT subject, STATE state)
   {
     STATE oldState = getState(subject);
-    setState(subject, state);
     if (oldState != state)
     {
+      setState(subject, state);
       fireEvent(new StateChangedEvent(subject, oldState, state));
     }
   }

1d29703dcd3d365424a061b5b5da220dca3e1eaf
Eike Stepper <stepper@esc-net.de>
2009-12-27 11:16:03 +0000

    [256649]  Make the concept of "successful commit" explicit and persistent https://bugs.eclipse.org/bugs/show_bug.cgi?id=256649

--- plugins/org.eclipse.emf.cdo/src/org/eclipse/emf/cdo/transaction/CDOSavepoint.java
+++ plugins/org.eclipse.emf.cdo/src/org/eclipse/emf/cdo/transaction/CDOSavepoint.java
@@ -17,6 +17,7 @@ import org.eclipse.emf.cdo.common.revision.delta.CDORevisionDelta;
 import org.eclipse.emf.cdo.eresource.CDOResource;
 
 import java.util.Map;
+import java.util.concurrent.ConcurrentMap;
 
 /**
  * @author Eike Stepper
@@ -38,18 +39,46 @@ public interface CDOSavepoint extends CDOUserSavepoint
   public boolean isDirty();
 
   /**
-   * Return the list of new objects from this point.
-   * 
    * @since 3.0
    */
-  public Map<CDOID, CDOObject> getAllDirtyObjects();
+  public Map<CDOID, CDORevision> getBaseNewObjects();
 
   /**
-   * Return the list of new objects from this point without objects that are removed.
+   * @since 3.0
+   */
+  public Map<CDOID, CDOResource> getNewResources();
+
+  /**
+   * @since 3.0
+   */
+  public Map<CDOID, CDOObject> getNewObjects();
+
+  /**
+   * @since 3.0
+   */
+  public Map<CDOID, CDOObject> getDirtyObjects();
+
+  /**
+   * @since 3.0
+   */
+  public ConcurrentMap<CDOID, CDORevisionDelta> getRevisionDeltas();
+
+  /**
+   * @since 3.0
+   */
+  public Map<CDOID, CDOObject> getDetachedObjects();
+
+  /**
+   * Bug 283985 (Re-attachment)
    * 
    * @since 3.0
    */
-  public Map<CDOID, CDOObject> getAllNewObjects();
+  public Map<CDOID, CDOObject> getReattachedObjects();
+
+  /**
+   * @since 3.0
+   */
+  public Map<CDOID, CDORevision> getAllBaseNewObjects();
 
   /**
    * Return the list of new resources from this point without objects that are removed.
@@ -59,14 +88,18 @@ public interface CDOSavepoint extends CDOUserSavepoint
   public Map<CDOID, CDOResource> getAllNewResources();
 
   /**
+   * Return the list of new objects from this point without objects that are removed.
+   * 
    * @since 3.0
    */
-  public Map<CDOID, CDOObject> getAllDetachedObjects();
+  public Map<CDOID, CDOObject> getAllNewObjects();
 
   /**
+   * Return the list of new objects from this point.
+   * 
    * @since 3.0
    */
-  public Map<CDOID, CDORevision> getAllBaseNewObjects();
+  public Map<CDOID, CDOObject> getAllDirtyObjects();
 
   /**
    * Return the list of all deltas without objects that are removed.
@@ -74,4 +107,9 @@ public interface CDOSavepoint extends CDOUserSavepoint
    * @since 3.0
    */
   public Map<CDOID, CDORevisionDelta> getAllRevisionDeltas();
+
+  /**
+   * @since 3.0
+   */
+  public Map<CDOID, CDOObject> getAllDetachedObjects();
 }

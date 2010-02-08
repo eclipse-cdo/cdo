ca8090bc97bee16a43b8b8ca319cde8240c2ec5e
Eike Stepper <stepper@esc-net.de>
2010-02-08 14:46:05 +0000

    [301110] Supply an empty list instead of null for CDODeltaNotification REMOVE_MANY. https://bugs.eclipse.org/bugs/show_bug.cgi?id=301110

--- plugins/org.eclipse.emf.cdo/src/org/eclipse/emf/internal/cdo/CDODeltaNotificationImpl.java
+++ plugins/org.eclipse.emf.cdo/src/org/eclipse/emf/internal/cdo/CDODeltaNotificationImpl.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *    Simon McDuff - initial API and implementation
  *    Eike Stepper - maintenance
@@ -15,6 +15,7 @@ import org.eclipse.emf.cdo.CDODeltaNotification;
 import org.eclipse.emf.cdo.common.id.CDOID;
 import org.eclipse.emf.cdo.common.revision.delta.CDORevisionDelta;
 
+import org.eclipse.emf.common.util.ECollections;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.InternalEObject;
 import org.eclipse.emf.ecore.impl.ENotificationImpl;
@@ -187,7 +188,21 @@ public class CDODeltaNotificationImpl extends ENotificationImpl implements CDODe
   @Override
   public Object getOldValue()
   {
-    return adapt(super.getOldValue());
+    Object oldValue = super.getOldValue();
+    if (oldValue == null)
+    {
+      Object feature = getFeature();
+      if (feature instanceof EStructuralFeature)
+      {
+        EStructuralFeature structuralFeature = (EStructuralFeature)feature;
+        if (structuralFeature.isMany())
+        {
+          return ECollections.emptyEList();
+        }
+      }
+    }
+
+    return adapt(oldValue);
   }
 
   public Object adapt(Object object)

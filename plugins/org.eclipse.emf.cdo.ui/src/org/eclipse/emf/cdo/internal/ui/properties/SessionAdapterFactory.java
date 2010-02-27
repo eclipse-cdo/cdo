602dd36edaa6c00c5dd397c57474ad86ac80a583
Eike Stepper <stepper@esc-net.de>
2010-02-27 10:19:38 +0000

    [256936] Support for Offline Mode https://bugs.eclipse.org/bugs/show_bug.cgi?id=256936

---
+++ plugins/org.eclipse.emf.cdo.ui/src/org/eclipse/emf/cdo/internal/ui/properties/SessionAdapterFactory.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2004 - 2010 Eike Stepper (Berlin, Germany) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Eike Stepper - initial API and implementation
+ */
+package org.eclipse.emf.cdo.internal.ui.properties;
+
+import org.eclipse.emf.cdo.session.CDOSession;
+
+import org.eclipse.core.runtime.IAdapterFactory;
+import org.eclipse.ui.views.properties.IPropertySource;
+import org.eclipse.ui.views.properties.IPropertySourceProvider;
+
+/**
+ * @author Eike Stepper
+ */
+@SuppressWarnings("rawtypes")
+public class SessionAdapterFactory implements IAdapterFactory
+{
+  public static final Class[] CLASSES = { IPropertySourceProvider.class };
+
+  public SessionAdapterFactory()
+  {
+  }
+
+  public Object getAdapter(Object adaptableObject, Class adapterType)
+  {
+    if (adaptableObject instanceof CDOSession)
+    {
+      if (adapterType == CLASSES[0])
+      {
+        return new IPropertySourceProvider()
+        {
+          public IPropertySource getPropertySource(Object object)
+          {
+            return new CDOSessionPropertySource((CDOSession)object);
+          }
+        };
+      }
+    }
+
+    return null;
+  }
+
+  public Class[] getAdapterList()
+  {
+    return CLASSES;
+  }
+}

8bc74403990574c4d92b8bc0d5be05aa7c817619
Eike Stepper <stepper@esc-net.de>
2010-05-04 10:00:12 +0000

    M7 prep

---
+++ plugins/org.eclipse.emf.cdo.tests/src/org/eclipse/emf/cdo/tests/MemoryLeakTest.java
@@ -0,0 +1,135 @@
+/**
+ * Copyright (c) 2004 - 2010 Eike Stepper (Berlin, Germany) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Eike Stepper - initial API and implementation
+ */
+package org.eclipse.emf.cdo.tests;
+
+import org.eclipse.emf.cdo.session.CDOSession;
+import org.eclipse.emf.cdo.tests.model1.Category;
+import org.eclipse.emf.cdo.tests.model1.Company;
+import org.eclipse.emf.cdo.tests.model1.Product1;
+import org.eclipse.emf.cdo.transaction.CDOTransaction;
+import org.eclipse.emf.cdo.util.CDOUtil;
+
+import org.eclipse.net4j.util.io.IOUtil;
+
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.resource.Resource;
+
+/**
+ * @author Eike Stepper
+ */
+public class MemoryLeakTest extends AbstractCDOTest
+{
+  private static final int LEVELS = 5;
+
+  private static final int CATEGORIES = 5;
+
+  private static final int PRODUCTS = 10;
+
+  protected Company company;
+
+  @Override
+  protected void doSetUp() throws Exception
+  {
+    // disableConsole();
+    super.doSetUp();
+
+    CDOSession session = openSession();
+    CDOTransaction transaction = session.openTransaction();
+    Resource resource = transaction.createResource("res1");
+
+    company = getModel1Factory().createCompany();
+    resource.getContents().add(company);
+    commit();
+  }
+
+  @Override
+  protected void doTearDown() throws Exception
+  {
+    // enableConsole();
+    super.doTearDown();
+  }
+
+  protected void commit()
+  {
+    CDOTransaction transaction = (CDOTransaction)CDOUtil.getCDOObject(company).cdoView();
+    transaction.commit();
+  }
+
+  public void testLargeModel() throws Exception
+  {
+    Category category = getModel1Factory().createCategory();
+    category.setName("ROOT");
+    company.getCategories().add(category);
+    commit();
+
+    try
+    {
+      createModel(category, LEVELS);
+    }
+    catch (OutOfMemoryError error)
+    {
+      error.printStackTrace();
+    }
+  }
+
+  private void createModel(Category parent, int levels)
+  {
+    IOUtil.OUT().println(parent + ": " + (LEVELS - levels));
+
+    EList<Category> categories = parent.getCategories();
+    for (int i = 0; i < CATEGORIES; i++)
+    {
+      Category category = getModel1Factory().createCategory();
+      category.setName("Category" + levels + "-" + i);
+      categories.add(category);
+    }
+
+    EList<Product1> products = parent.getProducts();
+    for (int i = 0; i < PRODUCTS; i++)
+    {
+      Product1 product = getModel1Factory().createProduct1();
+      product.setName("Product" + levels + "-" + i);
+      products.add(product);
+    }
+
+    commit();
+
+    if (levels > 0)
+    {
+      for (Category category : categories)
+      {
+        createModel(category, levels - 1);
+      }
+    }
+  }
+
+  private Category selectCategory(EList<Category> categories)
+  {
+    int size = categories.size();
+    if (size == 0)
+    {
+      return null;
+    }
+
+    int index = (int)(Math.random() * size);
+    Category category = categories.get(index);
+    if (Math.random() >= .5d)
+    {
+      Category recurse = selectCategory(category.getCategories());
+      if (recurse != null)
+      {
+        return recurse;
+      }
+    }
+
+    return category;
+  }
+}
---
+++ plugins/org.eclipse.emf.cdo.tests/src/org/eclipse/emf/cdo/tests/OldValueNotificationTest.java
@@ -0,0 +1,87 @@
+/**
+ * Copyright (c) 2004 - 2010 Eike Stepper (Berlin, Germany) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Eike Stepper - initial API and implementation
+ */
+package org.eclipse.emf.cdo.tests;
+
+import org.eclipse.emf.cdo.eresource.CDOResource;
+import org.eclipse.emf.cdo.session.CDOSession;
+import org.eclipse.emf.cdo.tests.model1.Address;
+import org.eclipse.emf.cdo.tests.model2.SpecialPurchaseOrder;
+import org.eclipse.emf.cdo.transaction.CDOTransaction;
+import org.eclipse.emf.cdo.util.CDOUtil;
+import org.eclipse.emf.cdo.view.CDOAdapterPolicy;
+import org.eclipse.emf.cdo.view.CDOView;
+
+import org.eclipse.emf.common.notify.Notification;
+
+import java.util.Date;
+
+/**
+ *
+ */
+public class OldValueNotificationTest extends AbstractCDOTest
+{
+  public void testSameSession() throws Exception
+  {
+    final CDOSession session = openSession();
+    final CDOTransaction transaction = session.openTransaction();
+    transaction.options().addChangeSubscriptionPolicy(CDOAdapterPolicy.ALL);
+
+    Address address1 = getModel1Factory().createAddress();
+    address1.setCity("Basel");
+    address1.setStreet("Turnerstrasse 39");
+    address1.setName("Michael Szediwy");
+
+    SpecialPurchaseOrder order = getModel2Factory().createSpecialPurchaseOrder();
+    order.setDate(new Date());
+    order.setShippingAddress(address1);
+
+    final CDOResource resourceA = transaction.createResource("/test1");
+    resourceA.getContents().add(order);
+    transaction.commit();
+
+    final TestAdapter adapter = new TestAdapter()
+    {
+      @Override
+      public void notifyChanged(Notification notification)
+      {
+        super.notifyChanged(notification);
+      }
+    };
+
+    final CDOView view = session.openView();
+    SpecialPurchaseOrder roOrder = view.getObject(order);
+    System.out.println(CDOUtil.getCDOObject(roOrder).cdoState());
+    roOrder.eAdapters().add(adapter);
+
+    Address address2 = getModel1Factory().createAddress();
+    address2.setCity("Basel2");
+    address2.setStreet("Turnerstrasse 392");
+    address2.setName("Michael Szediwy2");
+
+    order.setShippingAddress(address2);
+
+    transaction.commit();
+
+    new PollingTimeOuter()
+    {
+      @Override
+      protected boolean successful()
+      {
+        // Commit notifications from the same session always have full deltas
+        Notification[] notifications = adapter.getNotifications();
+        return notifications.length == 1;
+      }
+    }.assertNoTimeOut();
+
+    Notification[] notifications = adapter.getNotifications();
+    notifications[0].getOldValue();
+  }
+}

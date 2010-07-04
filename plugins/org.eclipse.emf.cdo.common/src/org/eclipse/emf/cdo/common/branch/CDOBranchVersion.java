cee14b9afdfaca653cef09350ced57ccf29c8c2d
Eike Stepper <stepper@esc-net.de>
2010-07-04 11:51:34 +0000

    [318807] Add JavaDocs https://bugs.eclipse.org/bugs/show_bug.cgi?id=318807

--- plugins/org.eclipse.emf.cdo.common/src/org/eclipse/emf/cdo/common/branch/CDOBranchVersion.java
+++ plugins/org.eclipse.emf.cdo.common/src/org/eclipse/emf/cdo/common/branch/CDOBranchVersion.java
@@ -10,17 +10,41 @@
  */
 package org.eclipse.emf.cdo.common.branch;
 
+import org.eclipse.emf.cdo.common.id.CDOID;
+import org.eclipse.emf.cdo.common.revision.CDORevision;
+import org.eclipse.emf.cdo.common.revision.CDORevisionManager;
+
 /**
+ * A pair of a {@link #getBranch() branch} and an integer {@link #getVersion() version} number.
+ * <p>
+ * It is often used in the context of
+ * {@link CDORevisionManager#getRevisionByVersion(CDOID, CDOBranchVersion, int, boolean)
+ * CDORevisionManager.getRevisionByVersion()}.
+ * 
+ * @see CDOBranch#getVersion(int)
  * @author Eike Stepper
  * @since 3.0
  */
 public interface CDOBranchVersion
 {
+  /**
+   * The fixed special version number <i>unspecified</i>.
+   */
   public static final int UNSPECIFIED_VERSION = 0;
 
+  /**
+   * The fixed version number that is assigned to the first {@link CDORevision revision} of an {@link CDOID object} that
+   * is committed to a particular branch .
+   */
   public static final int FIRST_VERSION = 1;
 
+  /**
+   * Returns the branch of this branch version.
+   */
   public CDOBranch getBranch();
 
+  /**
+   * Returns the version number of this branch version.
+   */
   public int getVersion();
 }

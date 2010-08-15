173755e98f7ca0610de181f0aa4ba670e92b91eb
Martin Fluegge <martin.fluegge@gmx.de>
2010-08-15 19:20:37 +0000

    [321024] [Dawn] DawnCreationWizard fails if the page for the semantic resource is not entered https://bugs.eclipse.org/bugs/show_bug.cgi?id=321024

--- plugins/org.eclipse.emf.cdo.dawn/src/org/eclipse/emf/cdo/dawn/transaction/DawnTransactionChangeRecorder.java
+++ plugins/org.eclipse.emf.cdo.dawn/src/org/eclipse/emf/cdo/dawn/transaction/DawnTransactionChangeRecorder.java
@@ -1,12 +1,12 @@
 package org.eclipse.emf.cdo.dawn.transaction;
 
+import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.transaction.impl.InternalTransactionalEditingDomain;
 import org.eclipse.emf.transaction.impl.TransactionChangeRecorder;
 
 public class DawnTransactionChangeRecorder extends TransactionChangeRecorder
 {
-
   public DawnTransactionChangeRecorder(InternalTransactionalEditingDomain domain, ResourceSet rset)
   {
     super(domain, rset);
@@ -16,5 +16,18 @@ public class DawnTransactionChangeRecorder extends TransactionChangeRecorder
   protected void assertWriting()
   {
     // Do nothing because we do not want to react an the assert. Every trnasaction is handle be CDO itself
+  }
+
+  /**
+   * @since 1.0
+   */
+  @Override
+  public void notifyChanged(Notification notification)
+  {
+    if (!(notification.getOldValue() instanceof ResourceSet && notification.getNewValue() == null && notification
+        .getEventType() == Notification.SET))
+    {
+      super.notifyChanged(notification);
+    }
   }
 }

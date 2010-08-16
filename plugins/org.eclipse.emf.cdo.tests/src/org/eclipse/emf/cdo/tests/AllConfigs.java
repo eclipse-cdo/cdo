d688f0a55871787df8c9b822ff02c7d3aa44568e
Eike Stepper <stepper@esc-net.de>
2010-08-16 11:58:57 +0000

    [322754] NullPointerException after deleting a resource https://bugs.eclipse.org/bugs/show_bug.cgi?id=322754

--- plugins/org.eclipse.emf.cdo.tests/src/org/eclipse/emf/cdo/tests/AllConfigs.java
+++ plugins/org.eclipse.emf.cdo.tests/src/org/eclipse/emf/cdo/tests/AllConfigs.java
@@ -83,6 +83,7 @@ import org.eclipse.emf.cdo.tests.bugzilla.Bugzilla_318876_Test;
 import org.eclipse.emf.cdo.tests.bugzilla.Bugzilla_319836_Test;
 import org.eclipse.emf.cdo.tests.bugzilla.Bugzilla_320837_Test;
 import org.eclipse.emf.cdo.tests.bugzilla.Bugzilla_321699_Test;
+import org.eclipse.emf.cdo.tests.bugzilla.Bugzilla_322754_Test;
 import org.eclipse.emf.cdo.tests.config.impl.ConfigTest;
 import org.eclipse.emf.cdo.tests.config.impl.ConfigTestSuite;
 
@@ -229,6 +230,7 @@ public abstract class AllConfigs extends ConfigTestSuite
     testClasses.add(Bugzilla_319836_Test.class);
     testClasses.add(Bugzilla_320837_Test.class);
     testClasses.add(Bugzilla_321699_Test.class);
+    testClasses.add(Bugzilla_322754_Test.class);
 
     // TODO testClasses.add(NonCDOResourceTest.class);
     // TODO testClasses.add(GeneratedEcoreTest.class);
---
+++ plugins/org.eclipse.emf.cdo.tests/src/org/eclipse/emf/cdo/tests/bugzilla/Bugzilla_322754_Test.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2004 - 2010 Eike Stepper (Berlin, Germany) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Eike Stepper - initial API and implementation
+ */
+package org.eclipse.emf.cdo.tests.bugzilla;
+
+import org.eclipse.emf.cdo.eresource.CDOResource;
+import org.eclipse.emf.cdo.session.CDOSession;
+import org.eclipse.emf.cdo.tests.AbstractCDOTest;
+import org.eclipse.emf.cdo.transaction.CDOTransaction;
+
+/**
+ * Bug 322754 - NullPointerException after deleting a resource
+ * 
+ * @author Eike Stepper
+ */
+public class Bugzilla_322754_Test extends AbstractCDOTest
+{
+  public void testResourceDelete() throws Exception
+  {
+    CDOSession session = openSession();
+    CDOTransaction transaction = session.openTransaction();
+    CDOResource resource = transaction.createResource("/r1");
+
+    msg("Fill and commit a resource");
+    resource.getContents().add(getModel1Factory().createCompany());
+    resource.getContents().add(getModel1Factory().createCompany());
+    resource.getContents().add(getModel1Factory().createCompany());
+    resource.getContents().add(getModel1Factory().createCompany());
+    resource.getContents().add(getModel1Factory().createCompany());
+    resource.getContents().add(getModel1Factory().createCompany());
+    resource.getContents().add(getModel1Factory().createCompany());
+    resource.getContents().add(getModel1Factory().createCompany());
+    resource.getContents().add(getModel1Factory().createCompany());
+    resource.getContents().add(getModel1Factory().createCompany());
+    resource.getContents().add(getModel1Factory().createCompany());
+    resource.getContents().add(getModel1Factory().createCompany());
+    resource.getContents().add(getModel1Factory().createCompany());
+    resource.getContents().add(getModel1Factory().createCompany());
+    transaction.commit();
+
+    msg("Delete and commit the resource");
+    resource.delete(null);
+    transaction.commit();
+    session.close();
+  }
+}

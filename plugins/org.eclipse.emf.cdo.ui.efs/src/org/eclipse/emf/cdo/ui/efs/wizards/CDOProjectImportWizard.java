02332b1da70cb1351ba3fe689e4e601f0632f529
Martin Fluegge <martin.fluegge@gmx.de>
2010-09-05 17:49:59 +0000

    enhanced CDOProjectImportWizard

--- plugins/org.eclipse.emf.cdo.ui.efs/src/org/eclipse/emf/cdo/ui/efs/wizards/CDOProjectImportWizard.java
+++ plugins/org.eclipse.emf.cdo.ui.efs/src/org/eclipse/emf/cdo/ui/efs/wizards/CDOProjectImportWizard.java
@@ -1,19 +1,31 @@
 package org.eclipse.emf.cdo.ui.efs.wizards;
 
+import org.eclipse.net4j.util.ui.UIUtil;
 import org.eclipse.net4j.util.ui.container.ElementWizardComposite;
 import org.eclipse.net4j.util.ui.container.IElementWizard.ValidationContext;
 
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jface.dialogs.IMessageProvider;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IImportWizard;
 import org.eclipse.ui.IWorkbench;
+
+import java.net.URI;
 
 /**
  * @author Martin Fluegge
@@ -31,7 +43,33 @@ public class CDOProjectImportWizard extends Wizard implements IImportWizard
   @Override
   public boolean performFinish()
   {
-    return false;
+    IWorkspace workspace = ResourcesPlugin.getWorkspace();
+
+    String projectName = "cdo_test1";
+    IProject project = workspace.getRoot().getProject(projectName);
+    if (project.exists())
+    {
+      return false;
+    }
+
+    IProjectDescription description = workspace.newProjectDescription(projectName);
+    description.setLocationURI(URI.create("cdo.net4j.tcp://localhost/repo1/MAIN/@"));
+
+    try
+    {
+      project.create(description, new NullProgressMonitor());
+      if (!project.isOpen())
+      {
+        project.open(new NullProgressMonitor());
+      }
+    }
+    catch (CoreException ex)
+    {
+      ex.printStackTrace();
+      return false;
+    }
+
+    return true;
   }
 
   @Override
@@ -52,12 +90,36 @@ public class CDOProjectImportWizard extends Wizard implements IImportWizard
 
     public void createControl(Composite parent)
     {
-      Group group = new Group(parent, SWT.NONE);
+      Composite composite = new Composite(parent, SWT.FILL);
+      // FillLayout layout = new FillLayout(SWT.VERTICAL);
+      GridLayout layout = new GridLayout(1, true);
+
+      composite.setLayout(layout);
+      // composite.setLayout(new GridLayout(1, true));
+
+      Group repositoryGroup = new Group(composite, SWT.NONE);
+      repositoryGroup.setLayout(new GridLayout(2, false));
+      repositoryGroup.setLayoutData(UIUtil.createGridData(true, false));
+
+      createRepositoryControl(repositoryGroup);
+
+      Group group = new Group(composite, SWT.NONE);
       group.setText("Connection");
       group.setLayout(new FillLayout());
+      group.setLayoutData(UIUtil.createGridData(true, true));
 
       new ElementWizardComposite.WithRadios(group, SWT.NONE, "org.eclipse.net4j.connectors", "Type:");
       setControl(group);
+    }
+
+    private void createRepositoryControl(Composite parent)
+    {
+      Label repositoryLabel = new Label(parent, SWT.NONE);
+      repositoryLabel.setText("Repository:");
+      repositoryLabel.setLayoutData(UIUtil.createGridData(false, false));
+
+      Text repositoryText = new Text(parent, SWT.BORDER);
+      repositoryText.setLayoutData(UIUtil.createGridData(true, false));
     }
 
     public void setValidationOK()

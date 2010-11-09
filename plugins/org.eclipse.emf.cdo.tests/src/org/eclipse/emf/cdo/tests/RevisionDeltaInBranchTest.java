0d8d7b41906463b3ea7e16796c23e833650824e3
Eike Stepper <stepper@esc-net.de>
2010-11-09 19:42:04 +0000

    [329025] [DB] Support branching for range-based mapping strategy https://bugs.eclipse.org/bugs/show_bug.cgi?id=329025

--- plugins/org.eclipse.emf.cdo.tests/src/org/eclipse/emf/cdo/tests/RevisionDeltaInBranchTest.java
+++ plugins/org.eclipse.emf.cdo.tests/src/org/eclipse/emf/cdo/tests/RevisionDeltaInBranchTest.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *    Eike Stepper - initial API and implementation
  */
@@ -29,6 +29,13 @@ import org.eclipse.emf.common.util.EList;
  */
 public class RevisionDeltaInBranchTest extends RevisionDeltaTest
 {
+  @Override
+  protected void doSetUp() throws Exception
+  {
+    super.doSetUp();
+    skipUnlessBranching();
+  }
+
   @Override
   protected void testStoreDelta(ListManipulator manipulator)
   {

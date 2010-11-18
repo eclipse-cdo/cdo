a37af3f634adcbcccf96460f11030699c6fe1835
Eike Stepper <stepper@esc-net.de>
2010-11-18 11:14:21 +0000

    added javadoc

--- plugins/org.eclipse.emf.cdo.common/src/org/eclipse/emf/cdo/internal/common/model/CDOPackageInfoImpl.java
+++ plugins/org.eclipse.emf.cdo.common/src/org/eclipse/emf/cdo/internal/common/model/CDOPackageInfoImpl.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *    Eike Stepper - initial API and implementation
  */
@@ -145,6 +145,11 @@ public class CDOPackageInfoImpl extends AdapterImpl implements InternalCDOPackag
   public boolean isResourcePackage()
   {
     return CDOModelUtil.isResourcePackage(getEPackage());
+  }
+
+  public boolean isTypePackage()
+  {
+    return CDOModelUtil.isTypesPackage(getEPackage());
   }
 
   public boolean isSystemPackage()

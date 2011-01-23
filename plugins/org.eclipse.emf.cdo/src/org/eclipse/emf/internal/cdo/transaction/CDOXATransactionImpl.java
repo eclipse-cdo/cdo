6a97fabaccaaa63cccd916f59d848c6121064b39
Eike Stepper <stepper@esc-net.de>
2011-01-23 10:12:33 +0000

    [335080] Make CDOView thread-safe https://bugs.eclipse.org/bugs/show_bug.cgi?id=335080

--- plugins/org.eclipse.emf.cdo/src/org/eclipse/emf/internal/cdo/transaction/CDOXATransactionImpl.java
+++ plugins/org.eclipse.emf.cdo/src/org/eclipse/emf/internal/cdo/transaction/CDOXATransactionImpl.java
@@ -424,6 +424,7 @@ public class CDOXATransactionImpl implements InternalCDOXATransaction
     return new CDOXASavepointImpl(this, lastSavepoint);
   }
 
+  @SuppressWarnings("unused")
   protected final ExecutorService createExecutorService()
   {
     if (SEQUENTIAL_EXECUTION)

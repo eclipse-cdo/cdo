30a24d2432abed89e68bec689bfb64cc04a64f31
Caspar De Groot <caspar_d@nomagicasia.com>
2011-02-14 09:35:07 +0000

    [Bug 337092] MEMBranchesTCP launch config https://bugs.eclipse.org/bugs/show_bug.cgi?id=337092

---
+++ plugins/org.eclipse.emf.cdo.tests/src/org/eclipse/emf/cdo/tests/AllTestsMEMBranchesTCP.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2004 - 2011 Eike Stepper (Berlin, Germany) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Caspar De Groot - initial API and implementation
+ */
+package org.eclipse.emf.cdo.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * @author Caspar De Groot
+ */
+public class AllTestsMEMBranchesTCP extends AllConfigs
+{
+  public static Test suite()
+  {
+    return new AllTestsMEMBranchesTCP().getTestSuite(AllConfigs.class.getName());
+  }
+
+  @Override
+  protected void initConfigSuites(TestSuite parent)
+  {
+    addScenario(parent, COMBINED, MEM_BRANCHES, TCP, NATIVE);
+  }
+}

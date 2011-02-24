0a6c3a633b0acf87df55e5c3464b95c154944f1c
Eike Stepper <stepper@esc-net.de>
2011-02-24 09:07:41 +0000

    threedee

---
+++ trunk/incubation/org.eclipse.emf.cdo.threedee/src/org/eclipse/emf/cdo/threedee/bundle/Startup.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2004 - 2011 Eike Stepper (Berlin, Germany) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Eike Stepper - initial API and implementation
+ */
+package org.eclipse.emf.cdo.threedee.bundle;
+
+import org.eclipse.ui.IStartup;
+
+/**
+ * @author Eike Stepper
+ */
+public class Startup implements IStartup
+{
+  public void earlyStartup()
+  {
+    // Do nothing
+  }
+}

29317e42a63f863e3848e86a526c75114f91eb98
Eike Stepper <stepper@esc-net.de>
2011-06-07 05:51:05 +0000

    javadoc

--- plugins/org.eclipse.emf.cdo.tests.db/src/org/eclipse/emf/cdo/tests/db/AllTestsDBPsql.java
+++ plugins/org.eclipse.emf.cdo.tests.db/src/org/eclipse/emf/cdo/tests/db/AllTestsDBPsql.java
@@ -32,15 +32,14 @@ import java.sql.SQLException;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
-/*
- * Database preparation before test execution.
- * 1. create user sa (pass: sa)
- * 2. create databases cdodb1, authrepo, repo2
- *
- * Database creation/removal is avoided since takes too long and makes test-suite impractical.
- */
-
 /**
+ * Database preparation before test execution:
+ * <ol>
+ * <li>create user sa (pass: sa)
+ * <li>create databases cdodb1, authrepo, repo2
+ * </ol>
+ * Database creation/removal is avoided because it takes too long and makes the test-suite impractical.
+ * 
  * @author Victor Roldan Betancort
  */
 public class AllTestsDBPsql extends DBConfigs

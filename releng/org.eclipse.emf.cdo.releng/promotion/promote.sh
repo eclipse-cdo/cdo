e88fe6b0941598bc812213b97a457e5be113e5ef
Eike Stepper <stepper@esc-net.de>
2011-06-12 15:00:38 +0000

    promote.sh

---
+++ releng/org.eclipse.emf.cdo.releng/promotion/promote.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#/shared/common/apache-ant-1.7.1/bin/ant \
+#	-f /shared/modeling/tools/promotion/download-artifacts.xml \
+#	-Dbuild.url=https://hudson.eclipse.org/hudson/job/emf-cdo-integration/1565
+	
+#svn log --xml https://dev.eclipse.org/svnroot/modeling/org.eclipse.emf.cdo/trunk > svn.log
+
+for job in `ls jobs`
+do
+	if [ -f "jobs/$job/nextBuildNumber" ]
+	then
+		lastBuildNumber=`cat "jobs/$job/nextBuildNumber"`
+	else
+		lastBuildNumber=0
+	fi
+	
+	nextBuildNumber=`cat "/shared/jobs/$job/nextBuildNumber"`
+	if [ "$nextBuildNumber" != "$lastBuildNumber" ]
+	then
+		echo "Promoting a build..."
+	else
+		echo "Nothing to promote."
+	fi
+	
+done
---
+++ releng/org.eclipse.emf.cdo.releng/promotion/update.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+svn checkout https://dev.eclipse.org/svnroot/modeling/org.eclipse.emf.cdo/trunk/releng/org.eclipse.emf.cdo.releng/promotion

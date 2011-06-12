4f33b8acf8397ef2bbbdd321a137287477260da7
Eike Stepper <stepper@esc-net.de>
2011-06-12 15:04:50 +0000

    promote.sh

--- releng/org.eclipse.emf.cdo.releng/promotion/update.sh
+++ releng/org.eclipse.emf.cdo.releng/promotion/update.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-svn checkout https://dev.eclipse.org/svnroot/modeling/org.eclipse.emf.cdo/trunk/releng/org.eclipse.emf.cdo.releng/promotion
+# Initially on build.eclipse.org:
+#svn checkout https://dev.eclipse.org/svnroot/modeling/org.eclipse.emf.cdo/trunk/releng/org.eclipse.emf.cdo.releng/promotion
+
+svn update

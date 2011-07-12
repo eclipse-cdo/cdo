10200fc366f51a45ef72a230a12c845d8538c2c4
Eike Stepper <stepper@esc-net.de>
2011-07-12 16:24:38 +0000

    [351616] [releng] Provide platform-specific server downloads https://bugs.eclipse.org/bugs/show_bug.cgi?id=351616

---
+++ plugins/org.eclipse.emf.cdo.explorer/src/org/eclipse/emf/cdo/internal/explorer/CDOExplorerActionBarAdvisor.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2004 - 2011 Eike Stepper (Berlin, Germany) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Eike Stepper - initial API and implementation
+ */
+package org.eclipse.emf.cdo.internal.explorer;
+
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.MenuManager;
+import org.eclipse.ui.IWorkbenchActionConstants;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.actions.ActionFactory;
+import org.eclipse.ui.actions.ActionFactory.IWorkbenchAction;
+import org.eclipse.ui.application.ActionBarAdvisor;
+import org.eclipse.ui.application.IActionBarConfigurer;
+
+/**
+ * @author Eike Stepper
+ */
+public class CDOExplorerActionBarAdvisor extends ActionBarAdvisor
+{
+  private IWorkbenchAction introAction;
+
+  public CDOExplorerActionBarAdvisor(IActionBarConfigurer configurer)
+  {
+    super(configurer);
+  }
+
+  @Override
+  protected void makeActions(IWorkbenchWindow window)
+  {
+    introAction = ActionFactory.INTRO.create(window);
+    register(introAction);
+  }
+
+  @Override
+  protected void fillMenuBar(IMenuManager menuBar)
+  {
+    MenuManager helpMenu = new MenuManager("&Help", IWorkbenchActionConstants.M_HELP);
+    menuBar.add(helpMenu);
+
+    // Help
+    helpMenu.add(introAction);
+  }
+}
---
+++ plugins/org.eclipse.emf.cdo.explorer/src/org/eclipse/emf/cdo/internal/explorer/CDOExplorerWorkbenchWindowAdvisor.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2004 - 2011 Eike Stepper (Berlin, Germany) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Eike Stepper - initial API and implementation
+ */
+package org.eclipse.emf.cdo.internal.explorer;
+
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.ui.application.ActionBarAdvisor;
+import org.eclipse.ui.application.IActionBarConfigurer;
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+/**
+ * @author Eike Stepper
+ */
+public class CDOExplorerWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor
+{
+  public CDOExplorerWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer)
+  {
+    super(configurer);
+  }
+
+  @Override
+  public ActionBarAdvisor createActionBarAdvisor(IActionBarConfigurer configurer)
+  {
+    return new CDOExplorerActionBarAdvisor(configurer);
+  }
+
+  @Override
+  public void preWindowOpen()
+  {
+    IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
+    configurer.setInitialSize(new Point(700, 550));
+    configurer.setShowCoolBar(false);
+    configurer.setShowStatusLine(false);
+    configurer.setTitle("CDO Explorer");
+  }
+}

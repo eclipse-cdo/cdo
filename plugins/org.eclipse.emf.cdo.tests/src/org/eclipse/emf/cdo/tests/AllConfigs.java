1837d87cc1f72263d0ab4223ab6c96b455936028
Martin Fluegge <martin.fluegge@gmx.de>
2011-08-31 13:03:57 +0000

    [355923] [Legacy] Adapter notified with CDOLegacyAdapter https://bugs.eclipse.org/bugs/show_bug.cgi?id=355923

--- plugins/org.eclipse.emf.cdo.tests/src/org/eclipse/emf/cdo/tests/AllConfigs.java
+++ plugins/org.eclipse.emf.cdo.tests/src/org/eclipse/emf/cdo/tests/AllConfigs.java
@@ -238,5 +238,6 @@ public abstract class AllConfigs extends ConfigTestSuite
     testClasses.add(Bugzilla_351393_Test.class);
     testClasses.add(Bugzilla_352303_Test.class);
     testClasses.add(Bugzilla_354395_Test.class);
+    testClasses.add(Bugzilla_355915_Test.class);
   }
 }

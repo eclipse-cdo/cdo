3130628a6012756cf045d73a3d73f8a8a9cc0853
Eike Stepper <stepper@esc-net.de>
2011-09-03 08:30:16 +0000

    converted package.html to packe-info.java / added apiviz

---
+++ plugins/org.eclipse.emf.cdo.ui.ide/src/org/eclipse/emf/cdo/ui/ide/package-info.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2004 - 2011 Eike Stepper (Berlin, Germany) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *    Eike Stepper - initial API and implementation
+ */
+
+/**
+ * Reusable UI IDE components specific to the CDO Model Repository.
+ */
+package org.eclipse.emf.cdo.ui.ide;

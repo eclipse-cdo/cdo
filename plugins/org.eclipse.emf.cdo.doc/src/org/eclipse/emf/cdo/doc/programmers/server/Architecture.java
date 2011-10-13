a327cf6a3979dba8519657b753ec8838eff3e8fc
Eike Stepper <stepper@esc-net.de>
2011-10-13 09:24:34 +0000

    docs

--- plugins/org.eclipse.emf.cdo.doc/src/org/eclipse/emf/cdo/doc/programmers/server/Architecture.java
+++ plugins/org.eclipse.emf.cdo.doc/src/org/eclipse/emf/cdo/doc/programmers/server/Architecture.java
@@ -10,7 +10,40 @@
  */
 package org.eclipse.emf.cdo.doc.programmers.server;
 
+import org.eclipse.emf.cdo.CDOLock;
+import org.eclipse.emf.cdo.common.branch.CDOBranch;
+import org.eclipse.emf.cdo.common.branch.CDOBranchManager;
+import org.eclipse.emf.cdo.common.commit.CDOCommitInfo;
+import org.eclipse.emf.cdo.common.commit.CDOCommitInfoHandler;
+import org.eclipse.emf.cdo.common.commit.CDOCommitInfoManager;
+import org.eclipse.emf.cdo.common.model.CDOPackageRegistry;
+import org.eclipse.emf.cdo.common.revision.CDORevision;
+import org.eclipse.emf.cdo.common.revision.CDORevisionCache;
+import org.eclipse.emf.cdo.common.revision.CDORevisionManager;
+import org.eclipse.emf.cdo.doc.reference.StoreFeatures;
+import org.eclipse.emf.cdo.server.ILockingManager;
+import org.eclipse.emf.cdo.server.IQueryHandler;
+import org.eclipse.emf.cdo.server.IQueryHandlerProvider;
+import org.eclipse.emf.cdo.server.IRepository;
+import org.eclipse.emf.cdo.server.IRepository.ReadAccessHandler;
+import org.eclipse.emf.cdo.server.IRepository.WriteAccessHandler;
+import org.eclipse.emf.cdo.server.ISessionManager;
+import org.eclipse.emf.cdo.server.IStore;
+import org.eclipse.emf.cdo.server.IStoreAccessor;
+import org.eclipse.emf.cdo.server.IStoreChunkReader;
+import org.eclipse.emf.cdo.session.CDOSession;
+import org.eclipse.emf.cdo.spi.server.ISessionProtocol;
+
+import org.eclipse.net4j.util.container.IPluginContainer;
+import org.eclipse.net4j.util.factory.IFactory;
+import org.eclipse.net4j.util.om.OMPlatform;
+import org.eclipse.net4j.util.om.log.OMLogger;
+import org.eclipse.net4j.util.om.monitor.OMMonitor;
+import org.eclipse.net4j.util.om.pref.OMPreference;
+import org.eclipse.net4j.util.om.trace.OMTracer;
+
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
 
 /**
  * Understanding the Architecture of a Repository
@@ -33,6 +66,17 @@ public class Architecture
 {
   /**
    * OSGi
+   * <p>
+   * All components of CDO are implemented as <a href="http://www.osgi.org">OSGi</a> bundles. The core components of
+   * both clients and servers do not require OSGi to actually run to be functional, they can perfectly be operated
+   * stand-alone. If OSGi is running the setup and configuration of some CDO facilities is a little simpler than in
+   * stand-alone mode because the needed {@link IFactory factories} get automatically registered with the central
+   * {@link IPluginContainer wiring container}.
+   * <p>
+   * CDO utilizes an {@link OMPlatform operations and maintenance} framework to abstract common platform services such
+   * as {@link OMLogger logging}, {@link OMTracer tracing}, {@link OMMonitor monitoring} and {@link OMPreference
+   * configuration}. Without the need to depend on additional external libraries these services integrate seamlessly
+   * with OSGi, if available at runtime, or emulate similar functionality if running stand-alone.
    */
   public class OSGi
   {
@@ -40,6 +84,35 @@ public class Architecture
 
   /**
    * CDO Server Core
+   * <p>
+   * The core of a CDO server consists of one or more {@link IRepository repositories} each of which, in turn, consists
+   * of several generic (network and storage independent) <b>components</b>, such as:
+   * <p>
+   * <ul>
+   * <li>a {@link CDORevision revision} {@link CDORevisionManager manager} and {@link CDORevisionCache cache},
+   * <li>a {@link CDOBranch branch} {@link CDOBranchManager manager},
+   * <li>a {@link EPackage package} {@link CDOPackageRegistry registry},
+   * <li>a {@link CDOLock lock} {@link ILockingManager manager},
+   * <li>a {@link CDOSession session} {@link ISessionManager manager},
+   * <li>a {@link CDOCommitInfo commit info} {@link CDOCommitInfoManager manager},
+   * <li>a {@link IQueryHandler query handler} {@link IQueryHandlerProvider provider}.
+   * </ul>
+   * <p>
+   * In addition the following types of <b>handlers</b> can be hooked up with a repository:
+   * <p>
+   * <ul>
+   * <li>{@link ReadAccessHandler Read access} handlers,
+   * <li>{@link WriteAccessHandler Write access} handlers,
+   * <li>{@link CDOCommitInfoHandler Commit info} handlers.
+   * </ul>
+   * <p>
+   * All <b>persistent aspects</b> (the storage/retrieval of data in/from a database system) are fully abstracted
+   * through the service provider interfaces (SPI) {@link IStore}, {@link IStoreAccessor} and {@link IStoreChunkReader}.
+   * Concrete implementations are fully separated and can be plugged into the core as described in {@link Store}.
+   * <p>
+   * All <b>communication aspects</b> (the sending/receiving of signals to/from a network system) are fully abstracted
+   * through the service provider interface (SPI) {@link ISessionProtocol}. Concrete implementations are fully separated
+   * and can be plugged into the core as described in {@link Protocol}.
    */
   public class Core
   {
@@ -47,6 +120,12 @@ public class Architecture
 
   /**
    * CDO Store
+   * <p>
+   * A concrete storage adapter, an {@link IStore} implementation, operates on top of the generic {@link Core server
+   * core}. A number of such stores already ship with CDO, making it possible to connect a repository to all sorts of
+   * JDBC databases, Hibernate, Objectivity/DB, MongoDB or DB4O.
+   * 
+   * @see StoreFeatures
    */
   public class Store
   {
@@ -60,7 +139,7 @@ public class Architecture
   }
 
   /**
-   * Net4j
+   * Net4j Core
    */
   public class Net4j
   {

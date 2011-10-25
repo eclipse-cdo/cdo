95ff15132ffc4802802230122c6dcf254a58dc66
Martin Fluegge <martin.fluegge@gmx.de>
2011-10-25 12:30:12 +0200

    [359656] [Dawn] Provide collaborative extension for Graphiti editors https://bugs.eclipse.org/bugs/show_bug.cgi?id=359656

---
+++ plugins/org.eclipse.emf.cdo.dawn.examples.acore.graphiti.dawn/src/org/eclipse/emf/cdo/dawn/examples/acore/graphiti/dawn/DawnAcoreGraphitiCreationWizard.java
@@ -0,0 +1,14 @@
+package org.eclipse.emf.cdo.dawn.examples.acore.graphiti.dawn;
+
+import org.eclipse.emf.cdo.dawn.graphiti.wizards.DawnBasicGraphitiWizard;
+
+/**
+ * @author Martin Fluegge
+ */
+public class DawnAcoreGraphitiCreationWizard extends DawnBasicGraphitiWizard
+{
+  public DawnAcoreGraphitiCreationWizard()
+  {
+    super("acore", "acore_graphiti");
+  }
+}
---
+++ plugins/org.eclipse.emf.cdo.dawn.examples.acore.graphiti.dawn/src/org/eclipse/emf/cdo/dawn/examples/acore/graphiti/dawn/DawnAcoreGraphitiDiagramEditor.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2004 - 2011 Eike Stepper (Berlin, Germany) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *    Martin Fluegge - initial API and implementation
+ */
+package org.eclipse.emf.cdo.dawn.examples.acore.graphiti.dawn;
+
+import org.eclipse.emf.cdo.dawn.graphiti.editors.DawnGraphitiDiagramEditor;
+
+/**
+ * @author Martin Fluegge
+ */
+public class DawnAcoreGraphitiDiagramEditor extends DawnGraphitiDiagramEditor
+{
+
+}

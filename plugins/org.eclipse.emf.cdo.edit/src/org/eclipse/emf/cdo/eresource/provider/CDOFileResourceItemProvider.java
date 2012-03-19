adbb257973866c3c389a636251cb7fe9180ff763
Eike Stepper <stepper@esc-net.de>
2012-03-19 11:53:31 +0100

    Enhance getText() in Eresource item providers

--- plugins/org.eclipse.emf.cdo.edit/src/org/eclipse/emf/cdo/eresource/provider/CDOFileResourceItemProvider.java
+++ plugins/org.eclipse.emf.cdo.edit/src/org/eclipse/emf/cdo/eresource/provider/CDOFileResourceItemProvider.java
@@ -28,7 +28,7 @@ import java.util.List;
 /**
  * This is the item provider adapter for a {@link org.eclipse.emf.cdo.eresource.CDOFileResource} object. <!--
  * begin-user-doc -->
- * 
+ *
  * @since 4.1
  * @noextend This class is not intended to be subclassed by clients. <!-- end-user-doc -->
  * @generated
@@ -39,7 +39,7 @@ public class CDOFileResourceItemProvider extends CDOResourceLeafItemProvider imp
 {
   /**
    * This constructs an instance from a factory and a notifier. <!-- begin-user-doc --> <!-- end-user-doc -->
-   * 
+   *
    * @generated
    */
   public CDOFileResourceItemProvider(AdapterFactory adapterFactory)
@@ -49,7 +49,7 @@ public class CDOFileResourceItemProvider extends CDOResourceLeafItemProvider imp
 
   /**
    * This returns the property descriptors for the adapted class. <!-- begin-user-doc --> <!-- end-user-doc -->
-   * 
+   *
    * @generated
    */
   @Override
@@ -65,7 +65,7 @@ public class CDOFileResourceItemProvider extends CDOResourceLeafItemProvider imp
 
   /**
    * This returns CDOFileResource.gif. <!-- begin-user-doc --> <!-- end-user-doc -->
-   * 
+   *
    * @generated
    */
   @Override
@@ -76,21 +76,21 @@ public class CDOFileResourceItemProvider extends CDOResourceLeafItemProvider imp
 
   /**
    * This returns the label text for the adapted class. <!-- begin-user-doc --> <!-- end-user-doc -->
-   * 
-   * @generated
+   *
+   * @generated NOT
    */
   @Override
   public String getText(Object object)
   {
     String label = ((CDOFileResource<?>)object).getName();
     return label == null || label.length() == 0 ? getString("_UI_CDOFileResource_type") : //$NON-NLS-1$
-        getString("_UI_CDOFileResource_type") + " " + label; //$NON-NLS-1$ //$NON-NLS-2$
+        label;
   }
 
   /**
    * This handles model notifications by calling {@link #updateChildren} to update any cached children and by creating a
    * viewer notification, which it passes to {@link #fireNotifyChanged}. <!-- begin-user-doc --> <!-- end-user-doc -->
-   * 
+   *
    * @generated
    */
   @Override
@@ -103,7 +103,7 @@ public class CDOFileResourceItemProvider extends CDOResourceLeafItemProvider imp
   /**
    * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s describing the children that can be created under
    * this object. <!-- begin-user-doc --> <!-- end-user-doc -->
-   * 
+   *
    * @generated
    */
   @Override

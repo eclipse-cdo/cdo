3629bad6bcc17723ba9bbfc4249e2abac3686244
Eike Stepper <stepper@esc-net.de>
2012-04-05 14:48:45 +0200

    [369646] ClassCastException after setBranch when server cache contains partially loaded collections https://bugs.eclipse.org/bugs/show_bug.cgi?id=369646

---
+++ plugins/org.eclipse.emf.cdo.tests/src/org/eclipse/emf/cdo/tests/bugzilla/Bugzilla_369646_Test.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2004 - 2012 Eike Stepper (Berlin, Germany) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Eike Stepper - initial API and implementation
+ */
+package org.eclipse.emf.cdo.tests.bugzilla;
+
+import org.eclipse.emf.cdo.common.branch.CDOBranch;
+import org.eclipse.emf.cdo.eresource.CDOResource;
+import org.eclipse.emf.cdo.session.CDOSession;
+import org.eclipse.emf.cdo.tests.AbstractCDOTest;
+import org.eclipse.emf.cdo.tests.config.IRepositoryConfig;
+import org.eclipse.emf.cdo.tests.model1.Category;
+import org.eclipse.emf.cdo.transaction.CDOTransaction;
+
+/**
+ * Make timeouts in read-access requests configurable
+ * <p>
+ * See bug 369646.
+ *
+ * @author Eike Stepper
+ */
+public class Bugzilla_369646_Test extends AbstractCDOTest
+{
+  @Requires(IRepositoryConfig.CAPABILITY_BRANCHING)
+  public void testSetBranchWithPCL() throws Exception
+  {
+    // Set up a resource with 1 object as content
+    CDOSession session = openSession();
+    CDOTransaction transaction = session.openTransaction();
+    CDOResource res = transaction.createResource(getResourcePath("/test"));
+
+    Category c = getModel1Factory().createCategory();
+    c.setName("Test");
+
+    res.getContents().add(c);
+    transaction.commit();
+
+    // create two cascading branches
+    CDOBranch sub1 = transaction.getBranch().createBranch("sub1");
+    CDOBranch sub2 = sub1.createBranch("sub2");
+
+    // now delete the contents in the sub2 branch
+    transaction.setBranch(sub2);
+    res.getContents().remove(0);
+    transaction.commit();
+
+    // and now try to switch the transaction to the parent branch
+    transaction.setBranch(sub1);
+  }
+}

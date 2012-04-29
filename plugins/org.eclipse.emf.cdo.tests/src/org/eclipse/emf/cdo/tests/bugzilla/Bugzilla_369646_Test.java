71a2521ace56eb0c108442c00ea12dc55e36c792
Stefan Winkler <stefan@winklerweb.net>
2012-04-29 14:59:48 +0200

    [369646] [DB] ClassCastException after setBranch when server cache contains partially loaded collections https://bugs.eclipse.org/bugs/show_bug.cgi?id=369646
    Second test case

--- plugins/org.eclipse.emf.cdo.tests/src/org/eclipse/emf/cdo/tests/bugzilla/Bugzilla_369646_Test.java
+++ plugins/org.eclipse.emf.cdo.tests/src/org/eclipse/emf/cdo/tests/bugzilla/Bugzilla_369646_Test.java
@@ -17,6 +17,10 @@ import org.eclipse.emf.cdo.tests.AbstractCDOTest;
 import org.eclipse.emf.cdo.tests.config.IRepositoryConfig;
 import org.eclipse.emf.cdo.tests.model1.Category;
 import org.eclipse.emf.cdo.transaction.CDOTransaction;
+import org.eclipse.emf.cdo.util.CDOUtil;
+import org.eclipse.emf.cdo.view.CDOView;
+
+import org.eclipse.net4j.util.io.IOUtil;
 
 /**
  * Make timeouts in read-access requests configurable
@@ -28,8 +32,11 @@ import org.eclipse.emf.cdo.transaction.CDOTransaction;
 public class Bugzilla_369646_Test extends AbstractCDOTest
 {
   @Requires(IRepositoryConfig.CAPABILITY_BRANCHING)
-  public void testSetBranchWithPCL() throws Exception
+  public void testSetBranchWithSubBranches() throws Exception
   {
+    // Note: as of Bugzilla 369646, this fails with DBStore and rangebased-braching mapping strategy
+    // the reason is that this strategy uses partially loaded resources internally.
+
     // Set up a resource with 1 object as content
     CDOSession session = openSession();
     CDOTransaction transaction = session.openTransaction();
@@ -53,4 +60,59 @@ public class Bugzilla_369646_Test extends AbstractCDOTest
     // and now try to switch the transaction to the parent branch
     transaction.setBranch(sub1);
   }
+
+  @Requires(IRepositoryConfig.CAPABILITY_BRANCHING)
+  public void testSetBranchWithPCL() throws Exception
+  {
+
+    {
+      // Set up a resource with 10 objects as content
+      CDOSession session = openSession();
+      CDOTransaction transaction = session.openTransaction();
+      CDOResource res = transaction.createResource(getResourcePath("/test"));
+
+      Category cat = getModel1Factory().createCategory();
+      cat.setName("Container");
+      res.getContents().add(cat);
+
+      for (int i = 0; i < 10; i++)
+      {
+        Category c = getModel1Factory().createCategory();
+        c.setName("Test " + i);
+        cat.getCategories().add(c);
+      }
+
+      transaction.commit();
+
+      // create a branch
+      CDOBranch sub1 = transaction.getBranch().createBranch("sub1");
+      transaction.setBranch(sub1);
+
+      // modify list in branch
+      cat.getCategories().remove(3);
+      cat.getCategories().remove(7);
+
+      // commit
+      transaction.commit();
+      transaction.close();
+      session.close();
+    }
+
+    // now clear the cache on server
+    clearCache(getRepository().getRevisionManager());
+
+    // open a new session with PCL enabled
+    CDOSession session = openSession();
+    session.options().setCollectionLoadingPolicy(CDOUtil.createCollectionLoadingPolicy(1, 1));
+
+    // load the categroy in the resource (is now partially loaded)
+    CDOView view = session.openView();
+    CDOResource res = view.getResource(getResourcePath("/test"));
+    Category cat = (Category)res.getContents().get(0);
+
+    // and switch view target
+    view.setBranch(view.getBranch().getBranches()[0]);
+    IOUtil.OUT().println("Result: " + cat.getCategories());
+  }
+
 }

c621f5843cf8bc53f5ff7ec0ff2247ee279ef81d
Eike Stepper <stepper@esc-net.de>
2012-05-01 19:02:35 +0200

    Revert "Revert "[369646] [DB] ClassCastException after setBranch when server cache contains partially loaded collections  https://bugs.eclipse.org/bugs/show_bug.cgi?id=369646""
    This reverts commit 2dc48cc9854bdae83437a39708a0c825c526f9b6.

--- plugins/org.eclipse.emf.cdo.tests/src/org/eclipse/emf/cdo/tests/bugzilla/Bugzilla_369646_Test.java
+++ plugins/org.eclipse.emf.cdo.tests/src/org/eclipse/emf/cdo/tests/bugzilla/Bugzilla_369646_Test.java
@@ -15,6 +15,7 @@ import org.eclipse.emf.cdo.eresource.CDOResource;
 import org.eclipse.emf.cdo.session.CDOSession;
 import org.eclipse.emf.cdo.tests.AbstractCDOTest;
 import org.eclipse.emf.cdo.tests.config.IRepositoryConfig;
+import org.eclipse.emf.cdo.tests.config.impl.ConfigTest.CleanRepositoriesBefore;
 import org.eclipse.emf.cdo.tests.model1.Category;
 import org.eclipse.emf.cdo.transaction.CDOTransaction;
 import org.eclipse.emf.cdo.util.CDOUtil;
@@ -29,13 +30,14 @@ import org.eclipse.net4j.util.io.IOUtil;
  *
  * @author Eike Stepper
  */
+@CleanRepositoriesBefore
 public class Bugzilla_369646_Test extends AbstractCDOTest
 {
   @Requires(IRepositoryConfig.CAPABILITY_BRANCHING)
   public void testSetBranchWithSubBranches() throws Exception
   {
-    // Note: as of Bugzilla 369646, this fails with DBStore and rangebased-braching mapping strategy
-    // the reason is that this strategy uses partially loaded resources internally.
+    // Note: as of bug 369646, this fails with DBStore and range-based branching mapping strategy.
+    // The reason is that this strategy uses partially loaded resources internally.
 
     // Set up a resource with 1 object as content
     CDOSession session = openSession();
@@ -64,7 +66,6 @@ public class Bugzilla_369646_Test extends AbstractCDOTest
   @Requires(IRepositoryConfig.CAPABILITY_BRANCHING)
   public void testSetBranchWithPCL() throws Exception
   {
-
     {
       // Set up a resource with 10 objects as content
       CDOSession session = openSession();
@@ -94,7 +95,6 @@ public class Bugzilla_369646_Test extends AbstractCDOTest
 
       // commit
       transaction.commit();
-      transaction.close();
       session.close();
     }
 
@@ -114,5 +114,4 @@ public class Bugzilla_369646_Test extends AbstractCDOTest
     view.setBranch(view.getBranch().getBranches()[0]);
     IOUtil.OUT().println("Result: " + cat.getCategories());
   }
-
 }

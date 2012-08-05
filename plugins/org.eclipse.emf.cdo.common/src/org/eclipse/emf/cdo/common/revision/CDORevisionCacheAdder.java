9041908c49238b4465bac220077d443de843bfb1
Eike Stepper <stepper@esc-net.de>
2012-08-05 12:58:45 +0200

    Mark CDORevisionCacheAdder extensible

--- plugins/org.eclipse.emf.cdo.common/src/org/eclipse/emf/cdo/common/revision/CDORevisionCacheAdder.java
+++ plugins/org.eclipse.emf.cdo.common/src/org/eclipse/emf/cdo/common/revision/CDORevisionCacheAdder.java
@@ -12,11 +12,9 @@ package org.eclipse.emf.cdo.common.revision;
 
 /**
  * An entity that {@link CDORevision revisions} can be added to.
- * 
+ *
  * @author Eike Stepper
  * @since 4.0
- * @noextend This interface is not intended to be extended by clients.
- * @noimplement This interface is not intended to be implemented by clients.
  */
 public interface CDORevisionCacheAdder
 {

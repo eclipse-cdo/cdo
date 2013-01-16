bf75a59ef46c76a0ececee5adab103d044a22393
Martin Taal <mtaal@elver.org>
2013-01-16 10:31:54 +0100

    Fixes issue [398275] - [Hibernate] Map entries are not deleted using delete-orphan

--- plugins/org.eclipse.emf.cdo.server.hibernate.teneo/src/org/eclipse/emf/cdo/server/hibernate/teneo/CDOOneToManyMapper.java
+++ plugins/org.eclipse.emf.cdo.server.hibernate.teneo/src/org/eclipse/emf/cdo/server/hibernate/teneo/CDOOneToManyMapper.java
@@ -10,9 +10,11 @@
  */
 package org.eclipse.emf.cdo.server.hibernate.teneo;
 
+import org.eclipse.emf.teneo.annotations.pamodel.PAnnotatedEReference;
 import org.eclipse.emf.teneo.hibernate.hbannotation.HbCascadeType;
 import org.eclipse.emf.teneo.hibernate.mapper.OneToManyMapper;
 import org.eclipse.emf.teneo.simpledom.Element;
+import org.eclipse.emf.teneo.util.StoreUtil;
 
 import java.util.List;
 
@@ -24,9 +26,27 @@ import java.util.List;
  */
 public class CDOOneToManyMapper extends OneToManyMapper
 {
+
+  private static ThreadLocal<Boolean> isMapMappedAsList = new ThreadLocal<Boolean>();
+
   @Override
   protected void addCascades(Element associationElement, List<HbCascadeType> cascades, boolean addDeleteOrphan)
   {
-    super.addCascades(associationElement, cascades, false);
+    super.addCascades(associationElement, cascades, isMapMappedAsList.get() ? addDeleteOrphan : false);
+  }
+
+  @Override
+  public void process(PAnnotatedEReference paReference)
+  {
+    // is a map and is not mapped as true map
+    isMapMappedAsList.set(StoreUtil.isMap(paReference.getModelEReference()) && !getHbmContext().isMapEMapAsTrueMap());
+    try
+    {
+      super.process(paReference);
+    }
+    finally
+    {
+      isMapMappedAsList.set(null);
+    }
   }
 }

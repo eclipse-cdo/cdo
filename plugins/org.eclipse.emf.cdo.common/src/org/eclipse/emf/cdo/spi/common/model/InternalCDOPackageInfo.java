b0ce7501cb2566d97733dd7a07d73b5d72f72134
Eike Stepper <stepper@esc-net.de>
2013-04-19 11:54:19 +0200

    [406068] Memory leak through CDOPackageInfo adapters on EPackages https://bugs.eclipse.org/bugs/show_bug.cgi?id=406068

--- plugins/org.eclipse.emf.cdo.common/src/org/eclipse/emf/cdo/spi/common/model/InternalCDOPackageInfo.java
+++ plugins/org.eclipse.emf.cdo.common/src/org/eclipse/emf/cdo/spi/common/model/InternalCDOPackageInfo.java
@@ -14,6 +14,7 @@ import org.eclipse.emf.cdo.common.model.CDOPackageInfo;
 import org.eclipse.emf.cdo.common.protocol.CDODataInput;
 import org.eclipse.emf.cdo.common.protocol.CDODataOutput;
 
+import org.eclipse.emf.common.notify.Adapter;
 import org.eclipse.emf.ecore.EPackage;
 
 import java.io.IOException;
@@ -26,7 +27,7 @@ import java.io.IOException;
  * @noextend This interface is not intended to be extended by clients.
  * @noimplement This interface is not intended to be implemented by clients.
  */
-public interface InternalCDOPackageInfo extends CDOPackageInfo
+public interface InternalCDOPackageInfo extends CDOPackageInfo, Adapter.Internal
 {
   public InternalCDOPackageUnit getPackageUnit();
 

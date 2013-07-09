c10efed04b77790622e586f166b03933a0964811
Eike Stepper <stepper@esc-net.de>
2013-07-09 14:03:36 +0200

    Add Git branch decorator

--- plugins/org.eclipse.emf.cdo.releng.gitbash/src/org/eclipse/emf/cdo/releng/gitbash/decorators/BranchDecorator.java
+++ plugins/org.eclipse.emf.cdo.releng.gitbash/src/org/eclipse/emf/cdo/releng/gitbash/decorators/BranchDecorator.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2012 Eike Stepper (Berlin, Germany) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Eike Stepper - initial API and implementation
+ */
 package org.eclipse.emf.cdo.releng.gitbash.decorators;
 
 import org.eclipse.jface.viewers.ILabelDecorator;
@@ -10,6 +20,9 @@ import org.eclipse.swt.graphics.Image;
 
 import java.io.IOException;
 
+/**
+ * @author Eike Stepper
+ */
 @SuppressWarnings("restriction")
 public class BranchDecorator implements ILabelDecorator
 {

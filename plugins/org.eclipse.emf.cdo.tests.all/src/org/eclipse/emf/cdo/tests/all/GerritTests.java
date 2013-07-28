90807b2dffb1bd8850fdf39c8ec436da7782104c
Eike Stepper <stepper@esc-net.de>
2013-07-28 08:46:14 +0200

    Adjust build for maintenance

---
+++ plugins/org.eclipse.emf.cdo.tests.all/src/org/eclipse/emf/cdo/tests/all/GerritTests.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2010-2013 Eike Stepper (Berlin, Germany) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Eike Stepper - initial API and implementation
+ *    Stefan Winkler - introduced variable mapping strategies
+ */
+package org.eclipse.emf.cdo.tests.all;
+
+import org.eclipse.emf.cdo.common.CDOCommonRepository.IDGenerationLocation;
+import org.eclipse.emf.cdo.tests.db.DBConfigs;
+import org.eclipse.emf.cdo.tests.db.H2Config;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * @author Eike Stepper
+ */
+public class GerritTests extends DBConfigs
+{
+  public static Test suite()
+  {
+    return new GerritTests().getTestSuite();
+  }
+
+  @Override
+  protected void initConfigSuites(TestSuite parent)
+  {
+    addScenario(parent, COMBINED, new H2Config(true, true, true, false, IDGenerationLocation.STORE), JVM, NATIVE);
+  }
+}

a724c99bccd1463395bb49a947bc82eadd591073
Ed Merks <ed.merks@gmail.com>
2013-10-28 11:05:34 +0100

    Improve setup related models

--- plugins/org.eclipse.emf.cdo.releng.setup.product/src/org/eclipse/emf/cdo/releng/setup/product/Application.java
+++ plugins/org.eclipse.emf.cdo.releng.setup.product/src/org/eclipse/emf/cdo/releng/setup/product/Application.java
@@ -10,6 +10,8 @@
  */
 package org.eclipse.emf.cdo.releng.setup.product;
 
+import org.eclipse.emf.cdo.releng.internal.setup.ui.ProgressLogDialog;
+import org.eclipse.emf.cdo.releng.internal.setup.ui.ResourceManager;
 import org.eclipse.emf.cdo.releng.setup.Preferences;
 import org.eclipse.emf.cdo.releng.setup.rcp.presentation.SetupEditorAdvisor;
 
@@ -18,13 +20,23 @@ import org.eclipse.emf.common.util.URI;
 
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 /**
  * @author Eike Stepper
@@ -78,14 +90,87 @@ public class Application implements IApplication
 
       return 0;
     }
-    catch (Exception ex)
+    catch (final Throwable ex)
     {
       Activator.log(ex);
+      new InternalErrorDialog(ex).open();
       return 1;
     }
   }
 
   public void stop()
   {
+  }
+
+  private static class InternalErrorDialog extends MessageDialog
+  {
+    private static int DETAILS_BUTTON_ID = 1;
+
+    private static final int TEXT_LINE_COUNT = 15;
+
+    private Throwable throwable;
+
+    private Text text;
+
+    public InternalErrorDialog(Throwable detail)
+    {
+      super(null, ProgressLogDialog.TITLE, ResourceManager.getPluginImage("org.eclipse.emf.cdo.releng.setup",
+          "icons/install_wiz.gif"), "Internal error" + System.getProperty("line.separator") + detail.getMessage(),
+          MessageDialog.ERROR, new String[] { IDialogConstants.OK_LABEL, IDialogConstants.SHOW_DETAILS_LABEL }, 0);
+      throwable = detail;
+      setShellStyle(getShellStyle() | SWT.APPLICATION_MODAL);
+    }
+
+    @Override
+    protected void buttonPressed(int buttonId)
+    {
+      if (buttonId == DETAILS_BUTTON_ID)
+      {
+        toggleDetailsArea();
+      }
+      else
+      {
+        super.buttonPressed(buttonId);
+      }
+    }
+
+    private void toggleDetailsArea()
+    {
+      Point windowSize = getShell().getSize();
+      Point oldSize = getContents().computeSize(SWT.DEFAULT, SWT.DEFAULT);
+
+      if (text != null)
+      {
+        text.dispose();
+        text = null;
+        getButton(DETAILS_BUTTON_ID).setText(IDialogConstants.SHOW_DETAILS_LABEL);
+      }
+      else
+      {
+        createDropDownText((Composite)getContents());
+        getButton(DETAILS_BUTTON_ID).setText(IDialogConstants.HIDE_DETAILS_LABEL);
+      }
+
+      Point newSize = getContents().computeSize(SWT.DEFAULT, SWT.DEFAULT);
+      getShell().setSize(new Point(windowSize.x, windowSize.y + newSize.y - oldSize.y));
+    }
+
+    protected void createDropDownText(Composite parent)
+    {
+      text = new Text(parent, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
+      text.setFont(parent.getFont());
+
+      StringWriter stringWriter = new StringWriter();
+      PrintWriter out = new PrintWriter(stringWriter);
+      throwable.printStackTrace(out);
+      out.close();
+      text.setText(stringWriter.toString());
+
+      GridData data = new GridData(GridData.HORIZONTAL_ALIGN_FILL | GridData.GRAB_HORIZONTAL
+          | GridData.VERTICAL_ALIGN_FILL | GridData.GRAB_VERTICAL);
+      data.heightHint = text.getLineHeight() * TEXT_LINE_COUNT;
+      data.horizontalSpan = 2;
+      text.setLayoutData(data);
+    }
   }
 }

08ad3e2379a4c5d783db97fbbab2572c3bfa749c
Christian W. Damus <give.a.damus@gmail.com>
2013-11-07 08:38:40 -0500

    [418454] [Admin] Client API and UI for managing repositories in a server https://bugs.eclipse.org/bugs/show_bug.cgi?id=418454
    Fix generated model EMF run-time version dependency.

--- plugins/org.eclipse.emf.cdo.server.admin/src/org/eclipse/emf/cdo/server/internal/admin/catalog/util/CatalogSwitch.java
+++ plugins/org.eclipse.emf.cdo.server.admin/src/org/eclipse/emf/cdo/server/internal/admin/catalog/util/CatalogSwitch.java
@@ -14,9 +14,10 @@ import org.eclipse.emf.cdo.server.internal.admin.catalog.CatalogPackage;
 import org.eclipse.emf.cdo.server.internal.admin.catalog.RepositoryCatalog;
 import org.eclipse.emf.cdo.server.internal.admin.catalog.RepositoryConfiguration;
 
+import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EPackage;
-import org.eclipse.emf.ecore.util.Switch;
+
+import java.util.List;
 
 /**
  * <!-- begin-user-doc -->
@@ -31,7 +32,7 @@ import org.eclipse.emf.ecore.util.Switch;
  * @see org.eclipse.emf.cdo.server.internal.admin.catalog.CatalogPackage
  * @generated
  */
-public class CatalogSwitch<T> extends Switch<T>
+public class CatalogSwitch<T>
 {
   /**
    * The cached model package
@@ -56,17 +57,15 @@ public class CatalogSwitch<T> extends Switch<T>
   }
 
   /**
-   * Checks whether this is a switch for the given package.
+   * Calls <code>caseXXX</code> for each class of the model until one returns a non null result; it yields that result.
    * <!-- begin-user-doc -->
    * <!-- end-user-doc -->
-   * @parameter ePackage the package in question.
-   * @return whether this is a switch for the given package.
+   * @return the first non-null result returned by a <code>caseXXX</code> call.
    * @generated
    */
-  @Override
-  protected boolean isSwitchFor(EPackage ePackage)
+  public T doSwitch(EObject theEObject)
   {
-    return ePackage == modelPackage;
+    return doSwitch(theEObject.eClass(), theEObject);
   }
 
   /**
@@ -76,7 +75,23 @@ public class CatalogSwitch<T> extends Switch<T>
    * @return the first non-null result returned by a <code>caseXXX</code> call.
    * @generated
    */
-  @Override
+  protected T doSwitch(EClass theEClass, EObject theEObject)
+  {
+    if (theEClass.eContainer() == modelPackage)
+    {
+      return doSwitch(theEClass.getClassifierID(), theEObject);
+    }
+    List<EClass> eSuperTypes = theEClass.getESuperTypes();
+    return eSuperTypes.isEmpty() ? defaultCase(theEObject) : doSwitch(eSuperTypes.get(0), theEObject);
+  }
+
+  /**
+   * Calls <code>caseXXX</code> for each class of the model until one returns a non null result; it yields that result.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return the first non-null result returned by a <code>caseXXX</code> call.
+   * @generated
+   */
   protected T doSwitch(int classifierID, EObject theEObject)
   {
     switch (classifierID)
@@ -149,7 +164,6 @@ public class CatalogSwitch<T> extends Switch<T>
    * @see #doSwitch(org.eclipse.emf.ecore.EObject)
    * @generated
    */
-  @Override
   public T defaultCase(EObject object)
   {
     return null;

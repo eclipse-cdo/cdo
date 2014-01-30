1c12f3d456a9506590ca95655a404cf147112494
Mathieu Velten <mathieu.velten@atos.net>
2014-01-30 17:21:54 +0100

    427244: Wrong behavior when moving an object of containment inside of the same resource (no CDOView attached) https://bugs.eclipse.org/bugs/show_bug.cgi?id=427244
    Signed-off-by: Mathieu Velten <mathieu.velten@atos.net>
    Change-Id: Ia4ea30d0755dbe9b217e4c5609578fd27b290a51

--- plugins/org.eclipse.emf.cdo/src/org/eclipse/emf/internal/cdo/CDOObjectImpl.java
+++ plugins/org.eclipse.emf.cdo/src/org/eclipse/emf/internal/cdo/CDOObjectImpl.java
@@ -765,7 +765,7 @@ public class CDOObjectImpl extends MinimalEStoreEObjectImpl implements InternalC
         : null;
 
     boolean moved = oldView != null && oldView == newView;
-    if (!moved && oldResource != null && !isResourceRoot)
+    if (!moved && oldResource != null && oldResource != newResource && !isResourceRoot)
     {
       oldResource.detached(this);
     }
@@ -773,7 +773,7 @@ public class CDOObjectImpl extends MinimalEStoreEObjectImpl implements InternalC
     int oldContainerFeatureID = eContainerFeatureID();
     eBasicSetContainer(newContainer, newContainerFeatureID);
 
-    if (!moved && oldResource != newResource && newResource != null)
+    if (!moved && newResource != null && newResource != oldResource)
     {
       newResource.attached(this);
     }

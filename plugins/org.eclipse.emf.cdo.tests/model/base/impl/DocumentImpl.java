bf704a59b4b0b01eaff38dffd82c850e9694ccfe
Eike Stepper <stepper@esc-net.de>
2016-06-07 14:54:49 +0200

    Adjust copyrights

--- plugins/org.eclipse.emf.cdo.tests/model/base/impl/DocumentImpl.java
+++ plugins/org.eclipse.emf.cdo.tests/model/base/impl/DocumentImpl.java
@@ -1,14 +1,22 @@
-/**
+/*
+ * Copyright (c) 2016 Eike Stepper (Berlin, Germany) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Eike Stepper - initial API and implementation
  */
 package base.impl;
+
+import org.eclipse.emf.internal.cdo.CDOObjectImpl;
+
+import org.eclipse.emf.ecore.EClass;
 
 import base.BasePackage;
 import base.Document;
 import base.Element;
-
-import org.eclipse.emf.ecore.EClass;
-
-import org.eclipse.emf.internal.cdo.CDOObjectImpl;
 
 /**
  * <!-- begin-user-doc -->
@@ -77,4 +85,4 @@ public class DocumentImpl extends CDOObjectImpl implements Document
     eSet(BasePackage.Literals.DOCUMENT__ROOT, newRoot);
   }
 
-} //DocumentImpl
+} // DocumentImpl
--- plugins/org.eclipse.emf.cdo.tests/model/base/impl/ElementImpl.java
+++ plugins/org.eclipse.emf.cdo.tests/model/base/impl/ElementImpl.java
@@ -1,15 +1,22 @@
-/**
+/*
+ * Copyright (c) 2016 Eike Stepper (Berlin, Germany) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Eike Stepper - initial API and implementation
  */
 package base.impl;
 
-import base.BasePackage;
-import base.Element;
+import org.eclipse.emf.internal.cdo.CDOObjectImpl;
 
 import org.eclipse.emf.common.util.EList;
-
 import org.eclipse.emf.ecore.EClass;
 
-import org.eclipse.emf.internal.cdo.CDOObjectImpl;
+import base.BasePackage;
+import base.Element;
 
 /**
  * <!-- begin-user-doc -->
@@ -90,4 +97,4 @@ public class ElementImpl extends CDOObjectImpl implements Element
     eSet(BasePackage.Literals.ELEMENT__PARENT, newParent);
   }
 
-} //ElementImpl
+} // ElementImpl

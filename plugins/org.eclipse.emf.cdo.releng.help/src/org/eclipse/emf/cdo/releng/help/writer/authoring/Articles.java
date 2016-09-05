09bac6a84cc74853a80ed1c257aba5e3106b4199
Eike Stepper <stepper@esc-net.de>
2016-09-05 07:25:02 +0200

    [500756] [Releng] Remove the .releng.* plugins and features from the site
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=500756

---
+++ plugins/org.eclipse.emf.cdo.releng.help/src/org/eclipse/emf/cdo/releng/help/writer/authoring/Articles.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2011-2013, 2015 Eike Stepper (Berlin, Germany) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Eike Stepper - initial API and implementation
+ */
+package org.eclipse.emf.cdo.releng.help.writer.authoring;
+
+/**
+ * Authoring Articles
+ * <p>
+ *
+ * @number 2
+ * @author Eike Stepper
+ */
+public class Articles
+{
+  /**
+   * Referencing Existing Content
+   */
+  public class Referencing
+  {
+    /**
+     * Referencing Categories
+     */
+    public class ReferencingCategories
+    {
+    }
+
+    /**
+     * Referencing Articles
+     */
+    public class ReferencingArticles
+    {
+    }
+
+    /**
+     * Referencing Chapters
+     */
+    public class ReferencingChapters
+    {
+    }
+
+    /**
+     * Reusing Content by Inlining
+     */
+    public class Inlining
+    {
+    }
+
+    /**
+     * Embedding External Content
+     */
+    public class Embedding
+    {
+      /**
+       * Embedding Java Code
+       */
+      public class Java
+      {
+      }
+
+      /**
+       * Embedding XML Code
+       */
+      public class XML
+      {
+      }
+
+      /**
+       * Embedding Excel Tables
+       */
+      public class Excel
+      {
+      }
+    }
+  }
+}

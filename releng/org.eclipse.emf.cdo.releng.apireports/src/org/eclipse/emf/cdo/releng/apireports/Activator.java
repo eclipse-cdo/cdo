9351b41517407f4ba7533e082fd0e2c2c33b4df4
Eike Stepper <stepper@esc-net.de>
2017-12-24 07:53:01 +0100

    [528979] Migrate build to Maven/Tycho
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=528979

---
+++ releng/org.eclipse.emf.cdo.releng.apireports/src/org/eclipse/emf/cdo/releng/apireports/Activator.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2012 Eike Stepper (Berlin, Germany) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Eike Stepper - initial API and implementation
+ */
+package org.eclipse.emf.cdo.releng.apireports;
+
+import org.eclipse.core.runtime.Plugin;
+
+import org.osgi.framework.BundleContext;
+
+/**
+ * @author Eike Stepper
+ */
+public class Activator extends Plugin
+{
+  public static final String PLUGIN_ID = "org.eclipse.emf.cdo.releng.apireports"; //$NON-NLS-1$
+
+  private static Activator plugin;
+
+  public Activator()
+  {
+  }
+
+  @Override
+  public void start(BundleContext context) throws Exception
+  {
+    super.start(context);
+    plugin = this;
+  }
+
+  @Override
+  public void stop(BundleContext context) throws Exception
+  {
+    plugin = null;
+    super.stop(context);
+  }
+
+  public static Activator getDefault()
+  {
+    return plugin;
+  }
+}
---
+++ releng/org.eclipse.emf.cdo.releng.apireports/src/org/eclipse/emf/cdo/releng/apireports/ApiReportsApplication.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2016 Eike Stepper (Berlin, Germany) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Eike Stepper - initial API and implementation
+ */
+package org.eclipse.emf.cdo.releng.apireports;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
+
+/**
+ * @author Eike Stepper
+ */
+public class ApiReportsApplication implements IApplication
+{
+  public Object start(IApplicationContext context) throws Exception
+  {
+    String reportFileName = null;
+    String baselineName = null;
+    String exclusionPatterns = null;
+
+    String[] arguments = (String[])context.getArguments().get(IApplicationContext.APPLICATION_ARGS);
+    if (arguments != null)
+    {
+      for (int i = 0; i < arguments.length; ++i)
+      {
+        String option = arguments[i];
+        if ("-reportFile".equals(option))
+        {
+          reportFileName = arguments[++i];
+        }
+        else if ("-baseline".equals(option))
+        {
+          baselineName = arguments[++i];
+        }
+        else if ("-exclusionPatterns".equals(option))
+        {
+          exclusionPatterns = arguments[++i];
+        }
+      }
+    }
+
+    IStatus result = ApiReportsGenerator.generate(reportFileName, baselineName, exclusionPatterns,
+        new NullProgressMonitor());
+    if (!result.isOK())
+    {
+      Throwable exception = result.getException();
+      if (exception instanceof Error)
+      {
+        throw (Error)exception;
+      }
+
+      if (exception instanceof Exception)
+      {
+        throw (Exception)exception;
+      }
+
+      throw new Exception(result.getMessage());
+    }
+
+    return null;
+  }
+
+  public void stop()
+  {
+  }
+}

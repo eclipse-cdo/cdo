20f29ab16df76f65f9454ffb70e713fc5354a689
Eike Stepper <stepper@esc-net.de>
2018-09-22 09:00:16 +0200

    Revert "[256856] Support model evolution"
    This reverts commit 3a01b3a8ad45368a29ce217fce7d36912d0a13b6.

--- plugins/org.eclipse.net4j.util/src/org/eclipse/net4j/util/collection/CollectionUtil.java
+++ plugins/org.eclipse.net4j.util/src/org/eclipse/net4j/util/collection/CollectionUtil.java
@@ -11,12 +11,8 @@
 package org.eclipse.net4j.util.collection;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Iterator;
-import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 /**
  * Various static helper methods.
@@ -28,90 +24,6 @@ public final class CollectionUtil
 {
   private CollectionUtil()
   {
-  }
-
-  /**
-   * @since 3.9
-   */
-  public static <K, V> Set<V> getSet(Map<K, Set<V>> map, K key)
-  {
-    Set<V> set = map.get(key);
-    if (set == null)
-    {
-      set = new LinkedHashSet<V>();
-      map.put(key, set);
-    }
-
-    return set;
-  }
-
-  /**
-   * @since 3.9
-   */
-  public static <K, V> boolean add(Map<K, Set<V>> map, K key, V value)
-  {
-    Set<V> set = getSet(map, key);
-    return set.add(value);
-  }
-
-  /**
-   * @since 3.9
-   */
-  public static <K, V> boolean addAll(Map<K, Set<V>> map, K key, Collection<? extends V> values)
-  {
-    Set<V> set = getSet(map, key);
-    return set.addAll(values);
-  }
-
-  /**
-   * @since 3.9
-   */
-  public static <K, V> boolean addAll(Map<K, Set<V>> map, Collection<? extends K> keys, V value)
-  {
-    boolean result = false;
-    for (K key : keys)
-    {
-      if (add(map, key, value))
-      {
-        result = true;
-      }
-    }
-
-    return result;
-  }
-
-  /**
-   * @since 3.9
-   */
-  public static <K, V> boolean addAll(Map<K, Set<V>> map, Map<? extends K, ? extends Collection<? extends V>> map2)
-  {
-    boolean result = false;
-    for (Map.Entry<? extends K, ? extends Collection<? extends V>> entry : map2.entrySet())
-    {
-      if (addAll(map, entry.getKey(), entry.getValue()))
-      {
-        result = true;
-      }
-    }
-
-    return result;
-  }
-
-  /**
-   * @since 3.9
-   */
-  public static <K, V> boolean putAll(Map<K, Set<V>> map, Map<? extends K, ? extends V> map2)
-  {
-    boolean result = false;
-    for (Map.Entry<? extends K, ? extends V> entry : map2.entrySet())
-    {
-      if (add(map, entry.getKey(), entry.getValue()))
-      {
-        result = true;
-      }
-    }
-
-    return result;
   }
 
   public static <T> Iterator<T> dump(Iterator<T> it)

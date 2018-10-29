c2b5d66a20ec9ca49cdfbd3f49f84e98a74a9f09
Eike Stepper <stepper@esc-net.de>
2018-10-29 12:40:43 +0100

    [Releng] Add org.eclipse.net4j.util.collection.RoundRobinIterator

---
+++ plugins/org.eclipse.net4j.util/src/org/eclipse/net4j/util/collection/RoundRobinIterator.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2018 Eike Stepper (Loehne, Germany) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Eike Stepper - initial API and implementation
+ */
+package org.eclipse.net4j.util.collection;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * @author Stepper
+ * @since 3.9
+ */
+public class RoundRobinIterator<T> implements Iterator<T>
+{
+  private final Collection<T> collection;
+
+  private Iterator<T> delegate;
+
+  public RoundRobinIterator(Collection<T> collection)
+  {
+    this.collection = collection;
+    if (!collection.isEmpty())
+    {
+      delegate = collection.iterator();
+    }
+  }
+
+  public boolean hasNext()
+  {
+    return delegate != null;
+  }
+
+  public T next()
+  {
+    if (delegate == null)
+    {
+      throw new NoSuchElementException();
+    }
+
+    if (!delegate.hasNext())
+    {
+      delegate = collection.iterator();
+    }
+
+    return delegate.next();
+  }
+
+  public void remove()
+  {
+    throw new UnsupportedOperationException();
+  }
+}

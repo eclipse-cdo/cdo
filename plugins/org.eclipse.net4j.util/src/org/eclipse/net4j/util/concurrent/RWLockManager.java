aef9b214730df4afedf25ba2b1e73dd9b9cf4569
Eike Stepper <stepper@esc-net.de>
2019-12-18 13:21:46 +0100

    [Releng] Fix Tycho build warning about overriding deprecated methods

--- plugins/org.eclipse.net4j.util/src/org/eclipse/net4j/util/concurrent/RWLockManager.java
+++ plugins/org.eclipse.net4j.util/src/org/eclipse/net4j/util/concurrent/RWLockManager.java
@@ -35,7 +35,7 @@ import java.util.Set;
  * @deprecated As of 3.2 use {@link RWOLockManager}.
  */
 @Deprecated
-@SuppressWarnings("all")
+@SuppressWarnings("all") // Needed for Tycho/Maven.
 public class RWLockManager<OBJECT, CONTEXT> extends Lifecycle implements IRWLockManager<OBJECT, CONTEXT>
 {
   private LockStrategy<OBJECT, CONTEXT> readLockStrategy = new LockStrategy<OBJECT, CONTEXT>()

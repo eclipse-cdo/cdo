41c0a62779d0f8d3cd570d95711b63dfb06010df
Eike Stepper <stepper@esc-net.de>
2020-06-30 07:32:21 +0200

    [Releng] Update aop.jar in org.eclipse.net4j.trace.aspectj

--- releng/org.eclipse.emf.cdo.releng/src/org/eclipse/emf/cdo/releng/PrintUnpublishedComponents.java
+++ releng/org.eclipse.emf.cdo.releng/src/org/eclipse/emf/cdo/releng/PrintUnpublishedComponents.java
@@ -64,7 +64,6 @@ public class PrintUnpublishedComponents
 
     try
     {
-      @SuppressWarnings("resource")
       BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
 
       String line;

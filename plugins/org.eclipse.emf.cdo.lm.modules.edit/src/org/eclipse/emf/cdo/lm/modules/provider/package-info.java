bd2dc0120daff1b71197f6aed1ca36361c6c2d87
Eike Stepper <stepper@esc-net.de>
2023-09-14 10:10:34 +0200

    [Releng] Regenerate documentation

---
+++ plugins/org.eclipse.emf.cdo.lm.modules.edit/src/org/eclipse/emf/cdo/lm/modules/provider/package-info.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2023 Eike Stepper (Loehne, Germany) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Eike Stepper - initial API and implementation
+ */
+
+/**
+ * The generated EMF edit support of the CDO Lifecycle Management Modules model.
+ */
+package org.eclipse.emf.cdo.lm.modules.provider;

eclipse-cdo/cdo

commit bf3754124acd26f4d00f4ef3273dbb94c9187773

[567178] CDOURIHandler.canHandle(URI) is too lax
https://bugs.eclipse.org/bugs/show_bug.cgi?id=567178

plugins/org.eclipse.emf.cdo.net4j/src/org/eclipse/emf/cdo/net4j/CDONet4jViewProvider.java
@@ -48,10 +48,13 @@ public abstract class CDONet4jViewProvider extends AbstractCDOViewProvider
    */
   protected final String transport;
 
+  private final String protocol;
+
   public CDONet4jViewProvider(String transport, int priority)
   {
     super("cdo\\.net4j\\." + transport + "://.*", priority);
     this.transport = transport;
+    protocol = "cdo.net4j." + transport;
   }
 
   @Override
@@ -98,13 +101,24 @@ public abstract class CDONet4jViewProvider extends AbstractCDOViewProvider
   @Override
   public URI getViewURI(URI uri)
   {
+    if (uri == null)
+    {
+      return null;
+    }
+
+    String scheme = uri.scheme();
+    if (scheme == null)
+    {
+      return null;
+    }
+
+    if (!scheme.equals(protocol))
+    {
+      return null;
+    }
+
     // Remove all (resource path) segments and leave only the repository name segment.
     return uri.trimSegments(uri.segmentCount() - 1);
-
-    // CDOURIData uriData = createURIData(uri);
-    // uriData.setResourcePath(null);
-    // uriData.setExtraParameters(null);
-    // return uriData.toURI();
   }
 
   @Override

plugins/org.eclipse.emf.cdo.explorer/src/org/eclipse/emf/cdo/internal/explorer/checkouts/CDOCheckoutViewProvider.java
@@ -22,6 +22,7 @@ import org.eclipse.net4j.util.lifecycle.LifecycleEventAdapter;
 import org.eclipse.net4j.util.ref.ReferenceMonitor;
 
 import org.eclipse.emf.common.notify.Adapter;
+import org.eclipse.emf.common.notify.Notifier;
 import org.eclipse.emf.common.notify.impl.AdapterImpl;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
@@ -29,6 +30,7 @@ import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 /**
@@ -91,12 +93,38 @@ public class CDOCheckoutViewProvider extends AbstractCDOViewProvider
     return null;
   }
 
+  @Override
+  public URI getViewURI(URI uri)
+  {
+    if (uri == null)
+    {
+      return null;
+    }
+
+    String scheme = uri.scheme();
+    if (scheme == null)
+    {
+      return null;
+    }
+
+    if (!scheme.equals(SCHEME))
+    {
+      return null;
+    }
+
+    return super.getViewURI(uri);
+  }
+
   public static void disposeResourceSet(ResourceSet resourceSet)
   {
-    CheckoutViewProviderAdapter adapter = CheckoutViewProviderAdapter.get(resourceSet);
-    if (adapter != null)
+    for (Iterator<Adapter> it = resourceSet.eAdapters().iterator(); it.hasNext();)
     {
-      adapter.dispose();
+      Adapter adapter = it.next();
+      if (adapter.getClass() == CheckoutViewProviderAdapter.class)
+      {
+        it.remove();
+        break;
+      }
     }
   }
 
@@ -182,22 +210,35 @@ public class CDOCheckoutViewProvider extends AbstractCDOViewProvider
       return (ResourceSet)super.getTarget();
     }
 
-    public void dispose()
+    @Override
+    public void setTarget(Notifier newTarget)
     {
-      CDOView[] array;
-      synchronized (views)
-      {
-        array = views.values().toArray(new CDOView[views.size()]);
-        views.clear();
-      }
-
-      for (CDOView view : array)
-      {
-        view.close();
-      }
-
       ResourceSet resourceSet = getTarget();
-      resourceSet.eAdapters().remove(this);
+      if (newTarget == resourceSet)
+      {
+        return;
+      }
+
+      if (newTarget == null)
+      {
+        CDOView[] array;
+        synchronized (views)
+        {
+          array = views.values().toArray(new CDOView[views.size()]);
+          views.clear();
+        }
+
+        for (CDOView view : array)
+        {
+          view.close();
+        }
+      }
+      else if (resourceSet != null)
+      {
+        throw new IllegalStateException("Illegal attempt to retarget CheckoutViewProviderAdapter to " + newTarget);
+      }
+
+      super.setTarget(newTarget);
     }
 
     public static CheckoutViewProviderAdapter get(ResourceSet resourceSet)

plugins/org.eclipse.emf.cdo/src/org/eclipse/emf/cdo/view/AbstractCDOViewProvider.java
@@ -144,6 +144,22 @@ public abstract class AbstractCDOViewProvider implements CDOViewProvider2
   @Override
   public String toString()
   {
-    return "CDOViewProvider[" + getRegex() + " --> " + ReflectUtil.getSimpleClassName(this) + ", " + getPriority() + "]";
+    return getDeclaredName() + "[" + getRegex() + " --> " + getActualName() + ", " + getPriority() + "]";
+  }
+
+  /**
+   * @since 4.12
+   */
+  protected String getDeclaredName()
+  {
+    return "CDOViewProvider";
+  }
+
+  /**
+   * @since 4.12
+   */
+  protected String getActualName()
+  {
+    return ReflectUtil.getSimpleClassName(this);
   }
 }

plugins/org.eclipse.emf.cdo/src/org/eclipse/emf/internal/cdo/view/CDOViewProviderRegistryImpl.java
@@ -23,6 +23,7 @@ import org.eclipse.emf.internal.cdo.bundle.OM;
 import org.eclipse.emf.internal.cdo.messages.Messages;
 import org.eclipse.emf.internal.cdo.session.CDOViewContainerImpl;
 
+import org.eclipse.net4j.util.ReflectUtil;
 import org.eclipse.net4j.util.StringUtil;
 import org.eclipse.net4j.util.WrappedException;
 import org.eclipse.net4j.util.collection.Pair;
@@ -353,6 +354,24 @@ public class CDOViewProviderRegistryImpl extends Container<CDOViewProvider> impl
       }
 
       return super.getPath(uri);
+    }
+
+    @Override
+    protected String getDeclaredName()
+    {
+      return "CDOViewProviderDescriptor";
+    }
+
+    @Override
+    protected String getActualName()
+    {
+      CDOViewProvider actualProvider = delegate;
+      if (actualProvider != null)
+      {
+        return ReflectUtil.getSimpleClassName(actualProvider);
+      }
+
+      return element.getAttribute("class");
     }
 
     private CDOViewProvider getViewProvider()

plugins/org.eclipse.emf.cdo/src/org/eclipse/emf/internal/cdo/view/PluginContainerViewProvider.java
@@ -90,6 +90,28 @@ public class PluginContainerViewProvider extends ManagedContainerViewProvider
     return CDOURIUtil.appendResourcePath(uri, path);
   }
 
+  @Override
+  public URI getViewURI(URI uri)
+  {
+    if (uri == null)
+    {
+      return null;
+    }
+
+    String scheme = uri.scheme();
+    if (scheme == null)
+    {
+      return null;
+    }
+
+    if (!scheme.equals(CDOURIUtil.PROTOCOL_NAME))
+    {
+      return null;
+    }
+
+    return super.getViewURI(uri);
+  }
+
   protected CDOView openView(CDOSession session, ResourceSet resourceSet)
   {
     return session.openTransaction(resourceSet);

commit 795a75775097ec89c8ce24a6204613b7dab5a121

Fix new implementation to handle ws:// and wss:// #25
Behavior was broken by commit
Commit bf3754124acd26f4d00f4ef3273dbb94c9187773
 [567178] CDOURIHandler.canHandle(URI) is too lax
 https://bugs.eclipse.org/bugs/show_bug.cgi?id=567178

Change-Id: Ief11cb0883c619c11d1373cfea372655fe55e91b
Signed-off-by: Maxime Porhel <maxime.porhel@obeo.fr>

plugins/org.eclipse.emf.cdo.net4j/src/org/eclipse/emf/cdo/net4j/CDONet4jViewProvider.java
@@ -164,7 +164,21 @@ public abstract class CDONet4jViewProvider extends AbstractCDOViewProvider
       CDOURIUtil.appendQueryParameter(query, CDOURIData.TRANSACTIONAL_PARAMETER, "true");
     }
 
-    URI uri = URI.createHierarchicalURI("cdo.net4j." + transport, authority, null, query.toString(), null).appendSegment(repositoryName);
+    String[] servicePathSegments = null;
+    if (authority.contains(CDOURIUtil.SEGMENT_SEPARATOR))
+    {
+      URI uri = URI.createURI(transport + "://" + authority);
+      authority = uri.authority();
+      servicePathSegments = uri.segments();
+    }
+
+    URI uri = URI.createHierarchicalURI("cdo.net4j." + transport, authority, null, query.toString(), null);
+
+    if (servicePathSegments != null && servicePathSegments.length > 0)
+    {
+      uri = uri.appendSegments(servicePathSegments);
+    }
+    uri = uri.appendSegment(repositoryName);
     return CDOURIUtil.appendResourcePath(uri, resourcePath);
   }
 
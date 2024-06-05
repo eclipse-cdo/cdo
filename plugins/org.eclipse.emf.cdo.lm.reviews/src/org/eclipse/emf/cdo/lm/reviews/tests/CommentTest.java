87388b646d22b67513da3532bcc9e47d3ac52dab
Eike Stepper <stepper@esc-net.de>
2024-06-05 12:49:55 +0200

    [Releng] Update copyrights

--- plugins/org.eclipse.emf.cdo.lm.reviews/src/org/eclipse/emf/cdo/lm/reviews/tests/CommentTest.java
+++ plugins/org.eclipse.emf.cdo.lm.reviews/src/org/eclipse/emf/cdo/lm/reviews/tests/CommentTest.java
@@ -1,4 +1,12 @@
-/**
+/*
+ * Copyright (c) 2023 Eike Stepper (Loehne, Germany) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Eike Stepper - initial API and implementation
  */
 package org.eclipse.emf.cdo.lm.reviews.tests;
 
@@ -50,7 +58,7 @@ public class CommentTest extends CommentableTest
   /**
    * <!-- begin-user-doc -->
    * <!-- end-user-doc -->
-   * 
+   *
    * @generated
    */
   @Override
@@ -62,7 +70,7 @@ public class CommentTest extends CommentableTest
   /**
    * <!-- begin-user-doc -->
    * <!-- end-user-doc -->
-   * 
+   *
    * @generated
    */
   @Override

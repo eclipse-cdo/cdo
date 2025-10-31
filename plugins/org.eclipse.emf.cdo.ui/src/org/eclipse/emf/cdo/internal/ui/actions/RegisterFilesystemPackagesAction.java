3ac1e66d4619880059732c31353cb15197d5399f
Eike Stepper <stepper@esc-net.de>
2025-10-31 12:00:28 +0100

    NPE in RegisterFilesystemPackages dialog #147

--- plugins/org.eclipse.emf.cdo.ui/src/org/eclipse/emf/cdo/internal/ui/actions/RegisterFilesystemPackagesAction.java
+++ plugins/org.eclipse.emf.cdo.ui/src/org/eclipse/emf/cdo/internal/ui/actions/RegisterFilesystemPackagesAction.java
@@ -43,7 +43,7 @@ public class RegisterFilesystemPackagesAction extends RegisterPackagesAction
       Messages.getString("RegisterFilesystemPackagesAction.4"), //$NON-NLS-1$
       Messages.getString("RegisterFilesystemPackagesAction.5") }; //$NON-NLS-1$
 
-  private static final String[] FILTER_EXTENSIONS = { "ecore", "xmi", "xml", null }; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+  private static final String[] FILTER_EXTENSIONS = { "ecore", "xmi", "xml", "*.*" }; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 
   public RegisterFilesystemPackagesAction(IWorkbenchPage page, CDOSession session)
   {

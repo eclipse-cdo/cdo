771c3b543a357d3367d30dd0e1705f03e47437b7
Eike Stepper <stepper@esc-net.de>
2025-11-13 17:18:40 +0100

    [Releng] Regenerate documentation

---
+++ plugins/org.eclipse.emf.cdo.server.db/src/org/eclipse/emf/cdo/server/db/evolution/package-info.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2011-2013, 2015, 2023 Eike Stepper (Loehne, Germany) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Eike Stepper - initial API and implementation
+ */
+
+/**
+ * Server concepts for dealing with model evolution and database migration.
+ */
+package org.eclipse.emf.cdo.server.db.evolution;
